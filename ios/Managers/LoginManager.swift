9fac482e1218f12994b7004d66f1e8a040146dc8
Lennie Stockman <lenniestockman@Lennies-MacBook-Pro.local>
2017-08-31 15:22:07 +0200

    save bearer correctly and send it to get extra data

--- ios/Managers/LoginManager.swift
+++ ios/Managers/LoginManager.swift
@@ -46,6 +46,10 @@ class LoginManager {
 
     }
     
+    public var isBearerStillValid: Bool {
+        return Date() < UserDefaults.standard.bearerExpiration
+    }
+    
     public func loginUser(email: String, password: String, completionHandler: @escaping (Bool?, NSError?) -> Void ) -> URLSessionTask {
         var request = URLRequest(url: URL(string: "https://givtapidebug.azurewebsites.net/oauth2/token")!)
         request.httpMethod = "POST"
@@ -63,20 +67,29 @@ class LoginManager {
             
             if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
                 print("statusCode should be 200, but is \(httpStatus.statusCode)")
-                print("response = \(response)")
+                print("response = \(String(describing: response))")
                 completionHandler(false, nil)
                 return
             }
             
             let responseString = String(data: data!, encoding: .utf8)
-            print("responseString = \(responseString)")
+            print("responseString = \(String(describing: responseString))")
             do
             {
                 let parsedData = try JSONSerialization.jsonObject(with: data!) as! [String:Any]
-                let currentData = parsedData
+                _ = parsedData
                 print(parsedData["access_token"]!)
                 if(parsedData["access_token"] != nil) {
                     UserDefaults.standard.bearerToken = parsedData["access_token"]! as! String
+                    let strTime = parsedData[".expires"] as! String
+                    let formatter = DateFormatter()
+                    formatter.dateFormat = "E, dd MMM yyyy HH:mm:ss Z"
+                    formatter.timeZone = TimeZone(abbreviation: "GMT")
+                    UserDefaults.standard.bearerExpiration = formatter.date(from: strTime)!
+                    
+                    self.getUserExt()
+                    
+                    print(UserDefaults.standard.bearerExpiration)
                    // UserDefaults.standard.bearerExpiration = parsedData[".expires"]! as! Date
                     completionHandler(true, nil)
                     return
@@ -92,5 +105,35 @@ class LoginManager {
         }
         task.resume()
         return task
+    }
+    
+    private func getUserExt() {
+        var request = URLRequest(url: URL(string:"https://givtapidebug.azurewebsites.net/api/UsersExtension")!)
+        request.httpMethod = "GET"
+        request.setValue("Bearer " + UserDefaults.standard.bearerToken, forHTTPHeaderField: "Authorization")
+        let urlSession = URLSession.shared
+        let task = urlSession.dataTask(with: request) { data, response, error -> Void in
+            if error != nil {
+                print(error! as NSError)
+                return
+            }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
+                print(response!)
+                return
+            }
+            
+            let responseString = String(data: data!, encoding: .utf8)
+            print(responseString)
+            do {
+                let parsedData = try JSONSerialization.jsonObject(with: data!) as! [String: Any]
+                _ = parsedData
+                print(parsedData)
+            } catch let err as NSError {
+                print(err)
+                return
+            }
+           
+        }.resume()
     }
 }
