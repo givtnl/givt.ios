ba95d4ac3068b444e9f5f6e6025c5609d44d27d9
Lennie Stockman <lenniestockman@Lennies-MacBook-Pro.local>
2017-09-21 13:56:16 +0200

    wrote extension for Date

--- ios/Date+helpers.swift
+++ ios/Date+helpers.swift
@@ -7,3 +7,47 @@
 //
 
 import Foundation
+
+extension Date {
+    public func getYear() -> Int {
+        let calendar = Calendar.current
+        return calendar.component(.year, from: self)
+    }
+    
+    public func getMonth() -> Int {
+        let calendar = Calendar.current
+        return calendar.component(.month, from: self)
+    }
+    
+    public func getDay() -> Int {
+        let calendar = Calendar.current
+        return calendar.component(.day, from: self)
+    }
+    
+    public func getHour() -> Int {
+        let calendar = Calendar.current
+        return calendar.component(.hour, from: self)
+    }
+    
+    public func getMinutes() -> Int {
+        let calendar = Calendar.current
+        return calendar.component(.minute, from: self)
+    }
+    
+    public func getSeconds() -> Int {
+        let calendar = Calendar.current
+        return calendar.component(.second, from: self)
+    }
+    
+    public func getMonthName() -> String {
+        let fmt = DateFormatter()
+        fmt.dateFormat = "MM"
+        return fmt.monthSymbols[self.getMonth() - 1]
+    }
+    
+    public func toString(_ format: String) -> String {
+        let fmt = DateFormatter()
+        fmt.dateFormat = format
+        return fmt.string(from: self)
+    }
+}
