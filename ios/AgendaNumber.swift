df5715c2bdf55b353c45e6064f9c6ac8c2259a8f
Lennie Stockman <lenniestockman@Lennies-MacBook-Pro.local>
2017-09-21 13:55:58 +0200

    added AgendaNumber control

--- ios/AgendaNumber.swift
+++ ios/AgendaNumber.swift
@@ -1,9 +1,20 @@
 //
 //  AgendaNumber.swift
-//  ios
+//  Playground
 //
-//  Created by Lennie Stockman on 19/09/17.
-//  Copyright © 2017 Maarten Vergouwe. All rights reserved.
+//  Created by Lennie Stockman on 13/09/17.
+//  Copyright © 2017 Lennie Stockman. All rights reserved.
 //
 
 import Foundation
+import UIKit
+
+@IBDesignable
+class AgendaNumber: UIView {
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+}
