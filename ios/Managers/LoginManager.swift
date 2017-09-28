4403f707251863121d9f1935842504d7a521734d
Lennie Stockman <lenniestockman@Lennies-MacBook-Pro.local>
2017-09-28 22:34:55 +0200

    added functions to register user

--- ios/Managers/LoginManager.swift
+++ ios/Managers/LoginManager.swift
@@ -1,3 +1,4 @@
+
 //
 //  LoginManager.swift
 //  ios
@@ -9,6 +10,8 @@
 import Foundation
 class LoginManager {
     static let shared = LoginManager()
+    private var _registrationUser: UserExt = UserExt()
+    
     
     private init() {
         print("loginmanager is created")
@@ -125,5 +128,102 @@ class LoginManager {
             }
            
         }.resume()
+    }
+    
+    func registerUser(_ user: RegistrationUser, completionHandler: @escaping (Bool) -> Void) {
+        
+        _registrationUser = UserExt()
+        _registrationUser.firstName = user.firstName
+        _registrationUser.lastName = user.lastName
+        _registrationUser.password = user.password
+        _registrationUser.email = user.email
+        
+        //TODO: log email to log service!
+        
+        completionHandler(true)
+    }
+    
+    func registerExtraDataFromUser(_ user: RegistrationUserData, completionHandler: @escaping (Bool) -> Void) {
+        _registrationUser.address = user.address
+        _registrationUser.city = user.city
+        _registrationUser.countryCode = user.countryCode
+        _registrationUser.mobileNumber = user.mobileNumber
+        _registrationUser.iban = user.iban
+        _registrationUser.postalCode = user.postalCode
+        
+        //checkTLD
+        var request = URLRequest(url: URL(string: _baseUrl + "/api/Users")!)
+        request.httpMethod = "POST"
+        let postString = "email=" + _registrationUser.email + "&password=" + _registrationUser.password
+        request.httpBody = postString.data(using: .utf8)
+        let urlSession = URLSession.shared
+        
+        let task = urlSession.dataTask(with: request) { data, response, error -> Void in
+            if error != nil {
+                completionHandler(false)
+                return
+            }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 && httpStatus.statusCode != 201 {
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                completionHandler(false)
+                return
+            }
+            
+            self._registrationUser.guid = String(bytes: data!, encoding: .utf8)!
+            UserDefaults.standard.guid = self._registrationUser.guid
+            self.registerAllData(completionHandler: { success in
+                if success {
+                    print("user succesfully registered")
+                    _ = self.loginUser(email: self._registrationUser.email, password: self._registrationUser.password, completionHandler: { (success, err) in
+                        if success {
+                            completionHandler(true)
+                        } else {
+                            completionHandler(false)
+                        }
+                    })
+                }
+            })
+            
+            
+        }
+        task.resume()
+    }
+    
+    func registerAllData(completionHandler: @escaping (Bool) -> Void) {
+        var request = URLRequest(url: URL(string: _baseUrl + "/api/UsersExtension")!)
+        request.httpMethod = "POST"
+        let postString =
+                "Guid=" + _registrationUser.guid +
+                "&IBAN=" + _registrationUser.iban +
+                    "&PhoneNumber=" + _registrationUser.mobileNumber +
+                    "&FirstName=" + _registrationUser.firstName +
+                    "&LastName=" + _registrationUser.lastName +
+                    "&Address=" + _registrationUser.address +
+                    "&City=" + _registrationUser.city +
+                    "&PostalCode=" + _registrationUser.postalCode +
+                    "&CountryCode=" + "BE"
+                    
+                
+        request.httpBody = postString.data(using: .utf8)
+        let urlSession = URLSession.shared
+        
+        let task = urlSession.dataTask(with: request) { data, response, error -> Void in
+            if error != nil {
+                return
+            }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 && httpStatus.statusCode != 201 {
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                return
+            }
+            
+            completionHandler(true)
+            
+           // _registrationUser.GUID = String(bytes: data!, encoding: .utf8)
+
+            
+        }
+        task.resume()
     }
 }
