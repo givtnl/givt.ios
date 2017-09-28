0553be49809b0eaef860fbc4530d4179e84124fb
Lennie Stockman <lenniestockman@Lennies-MacBook-Pro.local>
2017-09-28 22:31:53 +0200

    added some models

--- ios/RegistrationUser.swift
+++ ios/RegistrationUser.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+
+import UIKit
+
+class RegistrationUser {
+    var email: String
+    var password: String
+    var firstName: String
+    var lastName: String
+    
+    init(email: String, password: String, firstName: String, lastName: String) {
+        self.email = email
+        self.password = password
+        self.firstName = firstName
+        self.lastName = lastName
+    }
+}
+
+class RegistrationUserData {
+    var address: String = ""
+    var city: String = ""
+    var countryCode: String = ""
+    var iban: String = ""
+    var mobileNumber: String = ""
+    var postalCode: String = ""
+    
+    init(address: String, city: String, countryCode: String, iban: String, mobileNumber: String, postalCode: String) {
+        self.address = address
+        self.city = city
+        self.countryCode = countryCode
+        self.iban = iban
+        self.mobileNumber = mobileNumber
+        self.postalCode = postalCode
+    }
+    
+}
--- ios/UserExt.swift
+++ ios/UserExt.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+import UIKit
+
+class UserExt {
+    var email: String = ""
+    var password: String = ""
+    var firstName: String = ""
+    var lastName: String = ""
+    
+    var address: String = ""
+    var city: String = ""
+    var countryCode: String = ""
+    var iban: String = ""
+    var mobileNumber: String = ""
+    var postalCode: String = ""
+    var guid: String = ""
+    
+    var isTempUser: Bool = false
+    
+}
