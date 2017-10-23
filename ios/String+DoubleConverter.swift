cf04de7586453b2cd629c96582807d139f12d444
Lennie Stockman <lenniestockman@Lennies-MacBook-Pro.local>
2017-10-23 13:55:14 +0200

    added random string generator

--- ios/String+DoubleConverter.swift
+++ ios/String+DoubleConverter.swift
@@ -64,4 +64,15 @@ extension String{
             return Decimal(string: self.replacingOccurrences(of: ",", with: "."))!
         }
     }
+    
+    static func random(length: Int = 20) -> String {
+        let base = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        var randomString: String = ""
+        
+        for _ in 0..<length {
+            let randomValue = arc4random_uniform(UInt32(base.characters.count))
+            randomString += "\(base[base.index(base.startIndex, offsetBy: Int(randomValue))])"
+        }
+        return randomString
+    }
 }
