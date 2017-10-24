081abd8db993a3a565e0691354b432583550e154
Lennie Stockman <lenniestockman@Lennies-MacBook-Pro.local>
2017-10-24 11:32:34 +0200

    wrote extension (long live stackoverflow) to support when user taps on the main view: the keyboard hides

---
+++ ios/UIViewControllerExtensions.swift
@@ -0,0 +1,22 @@
+//
+//  UIViewControllerExtensions.swift
+//  ios
+//
+//  Created by Lennie Stockman on 24/10/17.
+//  Copyright © 2017 Givt. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
