e556fd78e45c8b5883439b0ac83018d3d09943da
Lennie Stockman <lenniestockman@Lennies-MacBook-Pro.local>
2017-10-25 09:28:42 +0200

    removed code we don't use anymore

--- ios/FinalRegistrationViewController.swift
+++ ios/FinalRegistrationViewController.swift
@@ -22,8 +22,6 @@ class FinalRegistrationViewController: UIViewController {
         self.navigationController?.setNavigationBarHidden(true, animated: false)
         nextButton.setTitle(NSLocalizedString("Next", comment: ""), for: .normal)
         titleLabel.text = NSLocalizedString("RegistrationSuccess", comment: "")
-        initTermsText()
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -36,30 +34,6 @@ class FinalRegistrationViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
     
-    func initTermsText() {
-        let attachment:NSTextAttachment = NSTextAttachment()
-        attachment.image = #imageLiteral(resourceName: "littleinfo.png")
-        attachment.bounds = CGRect(x: 0, y: (termsLabel.font.capHeight - (attachment.image?.size.height)! - 2).rounded(), width: (attachment.image?.size.width)!, height: (attachment.image?.size.height)!)
-        let attachmentString:NSAttributedString = NSAttributedString(attachment: attachment)
-        let myString:NSMutableAttributedString = NSMutableAttributedString(string: NSLocalizedString("AcceptTerms", comment: ""))
-        myString.append(attachmentString)
-        
-        termsLabel.attributedText = myString
-        let tap = UITapGestureRecognizer(target: self, action: #selector(openTerms))
-        termsLabel.addGestureRecognizer(tap)
-        termsLabel.isUserInteractionEnabled = true
-    }
-    
-    @objc func openTerms() {
-        let vc = storyboard?.instantiateViewController(withIdentifier: "TermsViewController") as! TermsViewController
-        vc.typeOfTerms = .termsAndConditions
-        self.present(vc, animated: true, completion: {
-            print("done terms")
-        })
-    }
-    
-    @IBAction func finish(_ sender: Any) {
-    }
     /*
     // MARK: - Navigation
 
