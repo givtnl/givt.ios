80540a0e390ddfd511c32e45e20b5b0731013ba5
Lennie Stockman <lenniestockman@Lennies-MacBook-Pro.local>
2017-11-14 13:22:30 +0100

    added images and empty controllers

---
+++ ios/Carousel/CarouselViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CarouselViewController.swift
+//  ios
+//
+//  Created by Lennie Stockman on 14/11/17.
+//  Copyright © 2017 Givt. All rights reserved.
+//
+
+import UIKit
+
+class CarouselViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
