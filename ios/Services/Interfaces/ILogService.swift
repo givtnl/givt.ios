d81346edacf5577e5ba607c09a98bbe33c49cf1e
Lennie <lenniestockman96@gmail.com>
2017-11-28 15:44:23 +0100

    created files for logging

---
+++ ios/Services/Interfaces/ILogService.swift
@@ -0,0 +1,9 @@
+//
+//  ILogService.swift
+//  ios
+//
+//  Created by Lennie Stockman on 28/11/17.
+//  Copyright © 2017 Givt. All rights reserved.
+//
+
+import Foundation
---
+++ ios/Services/LogService.swift
@@ -0,0 +1,9 @@
+//
+//  LogService.swift
+//  ios
+//
+//  Created by Lennie Stockman on 28/11/17.
+//  Copyright © 2017 Givt. All rights reserved.
+//
+
+import Foundation
