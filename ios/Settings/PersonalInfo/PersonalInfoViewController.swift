fe60600656000b0a8d3d8d08abc1a8fe19009c53
Lennie <lenniestockman96@gmail.com>
2017-12-04 09:24:26 +0100

    added the personal info view controller

--- ios/Settings/PersonalInfo/PersonalInfoViewController.swift
+++ ios/Settings/PersonalInfo/PersonalInfoViewController.swift
@@ -8,12 +8,73 @@
 
 import UIKit
 
-class PersonalInfoViewController: UIViewController {
+class PersonalInfoViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet var btnNext: CustomButton!
+    @IBOutlet var iban: CustomUITextField!
+    @IBOutlet var cellphone: UILabel!
+    @IBOutlet var postal: UILabel!
+    @IBOutlet var street: UILabel!
+    @IBOutlet var email: UILabel!
+    @IBOutlet var name: UILabel!
+    var countries = [Country]()
+    
+    var ibanNumber: String? = "" {
+        didSet {
+           // iban.text = ibanNumber?.replacingOccurrences(of: " ", with: "").separate(every: 4, with: " ")
+          //  iban.text = ibanNumber?.replacingOccurrences(of: " ", with: "").replace((.{4})/," ")
+        }
+    }
+    
+    func textFieldDidChange(_ textField: UITextField) {
+        if textField == iban {
+            ibanNumber = iban.text
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        iban.delegate = self
+        iban.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
 
+        
+        iban.text = ""
+        name.text = ""
+        email.text = ""
+        street.text = ""
+        postal.text = ""
+        cellphone.text = ""
         // Do any additional setup after loading the view.
+        
+        iban.placeholder = NSLocalizedString("IBANPlaceHolder", comment: "")
+        btnNext.setTitle(NSLocalizedString("ButtonChange", comment: ""), for: .normal)
+        
+        btnNext.isEnabled = false
+        
+        countries.append(Country(name: NSLocalizedString("Belgium", comment: ""), shortName: "BE", prefix: "+32"))
+        countries.append(Country(name: NSLocalizedString("Netherlands", comment: ""), shortName: "NL", prefix: "+31"))
+        countries.append(Country(name: NSLocalizedString("Germany", comment: ""), shortName: "DE", prefix: "+49"))
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if let user = UserDefaults.standard.userExt {
+            var country = ""
+            if let idx = Int(user.countryCode) {
+                country = countries[idx].shortName
+            } else {
+                country = user.countryCode
+            }
+            ibanNumber = user.iban
+            print(user.iban)
+            name.text = user.firstName + " " + user.lastName
+            email.text = user.email
+            street.text = user.address
+            postal.text = user.postalCode + " " + user.city + ", " + country
+            cellphone.text = user.mobileNumber
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,7 +82,10 @@ class PersonalInfoViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func goBack(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     /*
     // MARK: - Navigation
 
