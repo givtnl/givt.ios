36f33a9275a486b4c8c3c6eb68970a045183818c
Lennie <lenniestockman96@gmail.com>
2018-01-04 14:40:47 +0100

    Revert "improved validationhelper"
    This reverts commit 3e71822f3888dfd8e8a352192a9a65a121e122e6.

--- ios/ValidationHelper.swift
+++ ios/ValidationHelper.swift
@@ -73,13 +73,9 @@ class ValidationHelper {
     func isValidName(_ string: String) -> Bool {
         var allowedCharacters = CharacterSet.letters
         allowedCharacters.insert(" ")
-        allowedCharacters.insert("-")
-        allowedCharacters.insert("'")
         //remove all allowed characters. When rest is not 0, means that we have unwanted characters.
         let rest = string.trimmingCharacters(in: allowedCharacters)
-        let startsOrEndsWithIllegalCharacter = string.starts(with: " ") || string.starts(with: "-") || string.starts(with: "'") || string.last == " " || string.last == "-" || string.last == "'"
-        //if string start or ends with illegal character => return false
-        return rest.count == 0 && !startsOrEndsWithIllegalCharacter
+        return rest.count == 0
     }
     
     
