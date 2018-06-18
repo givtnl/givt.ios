03da7deefa574ab479cf19e21821943e1c3821e0
Lennie <lenniestockman96@gmail.com>
2018-06-18 12:09:40 +0200

    remove unused code

--- ios/ViewControllers/Give Flow/BaseScanViewController.swift
+++ ios/ViewControllers/Give Flow/BaseScanViewController.swift
@@ -109,12 +109,6 @@ class BaseScanViewController: UIViewController, GivtProcessedProtocol {
             parameters["nativeAppScheme"] = AppConstants.appScheme
             parameters["urlPart"] = AppConstants.returnUrlDir
             
-            /*
-            if let redirectAppScheme = GivtService.shared.customReturnAppScheme {
-                //parameters["redirectAppScheme"] = redirectAppScheme
-            }
- */
-            
             guard let jsonParameters = try? JSONSerialization.data(withJSONObject: parameters, options: JSONSerialization.WritingOptions.prettyPrinted) else {
                 return
             }
