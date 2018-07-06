44479a042bc207d10cd2b7a27c2952c0dd1d48fc
Lennie <lenniestockman96@gmail.com>
2018-07-06 11:35:02 +0200

    use my own library

--- ios/Services/GivtService.swift
+++ ios/Services/GivtService.swift
@@ -95,7 +95,7 @@ final class GivtService: NSObject {
         if let ma = organisation.MultiUseAllocations, ma.count > 0 {
             for m in ma {
                 let date = Date()
-                if let begin = CronExpression(cronString: m.dtBeginCron + " *")?.getNextRunDate(date), let end = CronExpression(cronString: m.dtEndCron + " *")?.getNextRunDate(date) {
+                if let begin = CronExpression(cronString: m.dtBeginCron)?.getNextRunDate(date), let end = CronExpression(cronString: m.dtEndCron)?.getNextRunDate(date) {
                     if end < begin {
                         self.log.info(message: "Could succesfully identify CRON-Allocation-Beacon")
                         return m.Name
