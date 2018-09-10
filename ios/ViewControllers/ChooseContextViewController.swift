7bcc652947bece5720133fb5b124d14b370eebc9
Lennie <lenniestockman96@gmail.com>
2018-09-10 14:15:16 +0200

    improved layout for iphone x too

--- ios/ViewControllers/ChooseContextViewController.swift
+++ ios/ViewControllers/ChooseContextViewController.swift
@@ -126,6 +126,37 @@ class ChooseContextViewController: UIViewController, UITableViewDelegate, UITabl
         let list = Context(title: NSLocalizedString("GivingContextCollectionBagList", comment: ""),subtitle: NSLocalizedString("SelectContextList", comment: ""), type: ContextType.manually, image: #imageLiteral(resourceName: "selectlist"))
         return !GivtManager.shared.hasGivtLocations() ? [collectionDevice, qr, list, location] : [collectionDevice, qr, location, list]
     }()
+    
+    @objc func pressedRow(_ tap: ContextTapGesture) {
+        let sb = UIStoryboard(name:"Main", bundle:nil)
+        DispatchQueue.main.async {
+            switch tap.contextType! {
+            case .collectionDevice:
+                if GivtManager.shared.isBluetoothEnabled || TARGET_OS_SIMULATOR != 0 {
+                    let vc = sb.instantiateViewController(withIdentifier: "scanView") as! ScanViewController
+                    self.navigationController!.show(vc, sender: nil)
+                } else {
+                    self.showBluetoothMessage()
+                }
+            case .qr:
+                let vc = sb.instantiateViewController(withIdentifier: "QRViewController") as! QRViewController
+                self.navigationController!.show(vc, sender: nil)
+            case .manually:
+                let vc = sb.instantiateViewController(withIdentifier: "ManualGivingViewController") as! ManualGivingViewController
+                self.navigationController!.show(vc, sender: nil)
+            case .events:
+                if !GivtManager.shared.hasGivtLocations() {
+                    let alert = UIAlertController(title: NSLocalizedString("GivtAtLocationDisabledTitle", comment: ""), message: NSLocalizedString("GivtAtLocationDisabledMessage", comment: ""), preferredStyle: UIAlertControllerStyle.alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+                    self.navigationController?.present(alert, animated: true, completion: nil)
+                } else {
+                    let story = UIStoryboard(name: "Event", bundle: nil)
+                    let vc = story.instantiateInitialViewController() as! EventViewController
+                    self.navigationController?.show(vc, sender: nil)
+                }
+            }
+        }
+    }
 
     
     @IBOutlet var tableView: UITableView!
@@ -145,12 +176,16 @@ class ChooseContextViewController: UIViewController, UITableViewDelegate, UITabl
             // Fallback on earlier versions
         }
         // Do any additional setup after loading the view.
-        
+        let hasGivtLocations = GivtManager.shared.hasGivtLocations()
         title = NSLocalizedString("SelectContext", comment: "")
         contexts.forEach { (context) in
             let containerView = UIView()
+            containerView.isUserInteractionEnabled = true
+            let tap = ContextTapGesture(target: self, action: #selector(pressedRow))
+            tap.contextType = context.type
+            containerView.addGestureRecognizer(tap)
             containerView.translatesAutoresizingMaskIntoConstraints = false
-            containerView.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+            containerView.backgroundColor = #colorLiteral(red: 0.8232886195, green: 0.8198277354, blue: 0.8529217839, alpha: 0.8036708048)
             contextsStackView.addArrangedSubview(containerView)
             
             let stackView = UIStackView()
@@ -158,21 +193,32 @@ class ChooseContextViewController: UIViewController, UITableViewDelegate, UITabl
             stackView.translatesAutoresizingMaskIntoConstraints = false
             stackView.axis = .horizontal
             containerView.addSubview(stackView)
-            stackView.topAnchor.constraint(equalTo: containerView.topAnchor).isActive = true
+            stackView.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 5).isActive = true
             stackView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 10).isActive = true
-            stackView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor).isActive = true
+            stackView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -5).isActive = true
             stackView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -10).isActive = true
             let img = UIImageView(image: context.image)
             img.contentMode = .scaleAspectFit
             stackView.addArrangedSubview(img)
             img.widthAnchor.constraint(lessThanOrEqualToConstant: 80).isActive = true
             let label = UILabel()
+            label.adjustsFontSizeToFitWidth = true
+            label.minimumScaleFactor = 0.6
             label.numberOfLines = 0
             label.textAlignment = .center
             label.translatesAutoresizingMaskIntoConstraints = false
             label.textColor = #colorLiteral(red: 0.1803921569, green: 0.1607843137, blue: 0.3411764706, alpha: 1)
             label.attributedText = makeAttributedString(title: context.title, subtitle: context.subtitle)
             stackView.addArrangedSubview(label)
+            let arrow = UIImageView(image: #imageLiteral(resourceName: "rightarrow-purple"))
+            arrow.translatesAutoresizingMaskIntoConstraints = false
+            arrow.contentMode = .scaleAspectFit
+            stackView.addArrangedSubview(arrow)
+            arrow.widthAnchor.constraint(equalToConstant: 12).isActive = true
+            
+            if !hasGivtLocations && context.type == .events {
+                containerView.alpha = 0.4
+            }
         }
     }
     
@@ -205,4 +251,8 @@ class ContextTableView: UITableView {
     override var intrinsicContentSize: CGSize {
         return contentSize
     }
+}
+
+class ContextTapGesture: UITapGestureRecognizer {
+    var contextType: ContextType!
 }
