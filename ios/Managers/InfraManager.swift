0cfac4952150eee78e4f45f5e5363b68fa3528d7
Lennie <lenniestockman96@gmail.com>
2018-09-13 15:40:55 +0200

    return .none if system can't evaluate policy

--- ios/Managers/InfraManager.swift
+++ ios/Managers/InfraManager.swift
@@ -52,14 +52,18 @@ class InfraManager {
     static func biometricType() -> BiometricType {
         let authContext = LAContext()
         if #available(iOS 11, *) {
-            let _ = authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil)
-            switch(authContext.biometryType) {
-            case .none:
+            var error: NSError?
+            if authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+                switch(authContext.biometryType) {
+                case .none:
+                    return .none
+                case .touchID:
+                    return .touch
+                case .faceID:
+                    return .face
+                }
+            } else {
                 return .none
-            case .touchID:
-                return .touch
-            case .faceID:
-                return .face
             }
         } else {
             return authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil) ? .touch : .none
