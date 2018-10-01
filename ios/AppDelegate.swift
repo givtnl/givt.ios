6922d0ba0b2f970f9cbd8bed5833cd1b99a4ae1d
Maarten Vergouwe <mvergouwe85@gmail.com>
2018-10-01 17:07:10 +0200

    Even more liberal parameter checking

--- ios/AppDelegate.swift
+++ ios/AppDelegate.swift
@@ -175,8 +175,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                     let appId = queryItems.first(where: { (item) -> Bool in item.name == "appid" })?.value
                 {
                     if let element = AppConstants.externalApps[appId], let imageString = element["logo"], let image = UIImage(named: imageString), let name = element["name"] {
-                        if mediumIdValue.count < 20 {
-                            LogService.shared.warning(message: "Wrong mediumid \"\(mediumIdValue)\" provided. Going to normal give flow")
+                        if mediumIdValue.count < 20 || GivtManager.shared.getOrganisationName(organisationNameSpace: String(mediumIdValue.prefix(20))) == nil {
+                            LogService.shared.warning(message: "Illegal mediumid \"\(mediumIdValue)\" provided. Going to normal give flow")
                         } else {
                             GivtManager.shared.externalIntegration = ExternalAppIntegration(name: name, logo: image, mediumId: mediumIdValue, appScheme: fromValue)
                             LogService.shared.info(message: "App scheme: \(fromValue) entering Givt-app with identifier \(mediumIdValue)")
