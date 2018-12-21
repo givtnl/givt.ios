a6c48f15a5c6c432ac002dce9de78210d213f6c5
Maarten Vergouwe <mvergouwe85@gmail.com>
2018-12-21 13:09:17 +0100

    icon for "App from A to Z" in the menu

--- ios/ViewControllers/Menu/SettingsViewController.swift
+++ ios/ViewControllers/Menu/SettingsViewController.swift
@@ -41,7 +41,7 @@ class SettingsViewController: BaseMenuViewController {
         items = []        
         let changeAccount = Setting(name: NSLocalizedString("LogoffSession", comment: ""), image: UIImage(named: "exit")!, callback: { self.logout() }, showArrow: false)
         
-        let appInfo = Setting(name: "The app from A to Z", image: UIImage(named: "info24")!, showBadge: FeatureManager.shared.showBadge, callback: { self.appInfo() })
+        let appInfo = Setting(name: "The app from A to Z", image: UIImage(named: "givt_atoz")!, showBadge: FeatureManager.shared.showBadge, callback: { self.appInfo() })
         let aboutGivt = Setting(name: NSLocalizedString("TitleAboutGivt", comment: ""), image: UIImage(named: "info24")!, callback: { self.about() })
         let shareGivt = Setting(name: NSLocalizedString("ShareGivtText", comment: ""), image: UIImage(named: "share")!, callback: { self.share() }, showArrow: false)
         
