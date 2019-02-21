2f54f00f725b691c36b5a63c349d8cdaca742543
Bjorn Derudder <bjorn.derudder@gmail.com>
2019-02-21 15:57:45 +0100

    Added checkamounts

--- ios/ViewControllers/Give Flow/AmountViewController.swift
+++ ios/ViewControllers/Give Flow/AmountViewController.swift
@@ -11,8 +11,6 @@ import MaterialShowcase
 
 class AmountViewController: UIViewController, UIGestureRecognizerDelegate, NavigationManagerDelegate, MaterialShowcaseDelegate {
     private var log: LogService = LogService.shared
-    @IBOutlet var widthConstraint: NSLayoutConstraint!
-    @IBOutlet var collectionView: UIView!
     private let slideFromRightAnimation = PresentFromRight()
     
     private var navigiationManager: NavigationManager = NavigationManager.shared
@@ -127,7 +125,7 @@ class AmountViewController: UIViewController, UIGestureRecognizerDelegate, Navig
     @IBOutlet var collectTwo: CollectionView!
     @IBOutlet var collectThree: CollectionView!
     
-    var currentAmount: CollectionView!
+    var currentCollect: CollectionView!
     
     var nuOfCollectsShown: Int {
         var count = 0
@@ -307,9 +305,9 @@ class AmountViewController: UIViewController, UIGestureRecognizerDelegate, Navig
         backItem.title = NSLocalizedString("Cancel", comment: "Annuleer")
         backItem.style = .plain
         backItem.setTitleTextAttributes([NSAttributedStringKey.font: UIFont(name: "Avenir-Heavy", size: 18)!], for: .normal)
-//        btnGive.setBackgroundColor(color: UIColor.init(rgb: 0xE3E2E7), forState: .disabled)
+        btnNext.setBackgroundColor(color: UIColor.init(rgb: 0xE3E2E7), forState: .disabled)
         self.navigationItem.backBarButtonItem = backItem
-//        checkAmounts()
+        checkAmounts()
         
         log.info(message:"Mandate signed: " + String(UserDefaults.standard.mandateSigned))
         
@@ -348,7 +346,7 @@ class AmountViewController: UIViewController, UIGestureRecognizerDelegate, Navig
     }
     
     @IBAction func addValue(sender:UIButton!) {
-        var currentAmountLabel = currentAmount.amountLabel!
+        var currentAmountLabel = currentCollect.amountLabel!
         
         if currentAmountLabel.text == "0" || pressedShortcutKey {
             currentAmountLabel.text = ""
@@ -372,21 +370,21 @@ class AmountViewController: UIViewController, UIGestureRecognizerDelegate, Navig
             return
         }
         currentAmountLabel.text = currentAmountLabel.text! + sender.currentTitle!;
-//        checkAmounts()
+        checkAmounts()
         pressedShortcutKey = false
     }
     
 
     @IBAction func setActiveCollection(_ sender: Any) {
-        currentAmount = sender as? CollectionView
+        currentCollect = sender as? CollectionView
         collectOne.active = false
         collectTwo.active = false
         collectThree.active = false
-        currentAmount.active = true
+        currentCollect.active = true
     }
     
     @IBAction func addShortcutValue(_ sender: Any) {
-        var currentAmountLabel = currentAmount.amountLabel!
+        var currentAmountLabel = currentCollect.amountLabel!
         
         let button = sender as! PresetButton
         
@@ -401,15 +399,15 @@ class AmountViewController: UIViewController, UIGestureRecognizerDelegate, Navig
                 self.log.info(message: "User used a custom amount preset")
             }
         }
-        //        checkAmounts()
+        checkAmounts()
         pressedShortcutKey = true
     }
     
     @IBAction func clearValue(sender: UIButton!){
-        let currentAmountLabel = currentAmount.amountLabel!
+        let currentAmountLabel = currentCollect.amountLabel!
         var amount: String = currentAmountLabel.text!
         if amount.count == 0 {
-//            checkAmounts()
+            checkAmounts()
             return
         }
 
@@ -418,14 +416,14 @@ class AmountViewController: UIViewController, UIGestureRecognizerDelegate, Navig
         if amount.count == 0 || pressedShortcutKey {
             currentAmountLabel.text = "0";
         }
-        //checkAmounts()
+        checkAmounts()
 
     }
 
     @IBAction func clearAll(_ sender: Any) {
-        var currentAmountLabel = currentAmount.amountLabel!
+        var currentAmountLabel = currentCollect.amountLabel!
         currentAmountLabel.text = "0";
-        //checkAmounts()
+        checkAmounts()
     }
     
 //    func clearAmounts() {
@@ -452,8 +450,8 @@ class AmountViewController: UIViewController, UIGestureRecognizerDelegate, Navig
             preferredStyle: UIAlertControllerStyle.alert)
         alert.addAction(UIAlertAction(title: NSLocalizedString("ChooseLowerAmount", comment: ""), style: .default, handler: {
             action in
-            self.currentAmount.amountLabel.text = String(UserDefaults.standard.amountLimit)
-            //self.checkAmounts()
+            self.currentCollect.amountLabel.text = String(UserDefaults.standard.amountLimit)
+            self.checkAmounts()
         }))
         if (LoginManager.shared.isFullyRegistered){
             alert.addAction(UIAlertAction(title: NSLocalizedString("ChangeGivingLimit", comment: ""), style: .cancel, handler: { action in
@@ -468,18 +466,18 @@ class AmountViewController: UIViewController, UIGestureRecognizerDelegate, Navig
         self.present(alert, animated: true, completion: nil)
     }
 
-//    func checkAmounts() {
-//        var amountsUnder50C = 0
-//        for index in 0..<numberOfCollects {
-//            let parsedDecimal = Decimal(string: (amountLabels[index].text!.replacingOccurrences(of: ",", with: ".")))!
-//            if parsedDecimal < 0.50 {
-//                amountsUnder50C += 1
-//            }
-//            btnNext.isEnabled = amountsUnder50C != numberOfCollects
-//        }
-//
-//        currentAmountLabel.textColor = Decimal(string: (currentAmountLabel.text!.replacingOccurrences(of: ",", with: ".")))! > Decimal(amountLimit) ? UIColor.init(rgb: 0xb91a24).withAlphaComponent(0.5) : UIColor.init(rgb: 0xD2D1D9)
-//    }
+    func checkAmounts() {
+        var amountsUnder50C = 0
+        for index in 0..<collectionViews.count {
+            let parsedDecimal = Decimal(string: (collectionViews[index].amountLabel.text!.replacingOccurrences(of: ",", with: ".")))!
+            if parsedDecimal < 0.50 {
+                amountsUnder50C += 1
+            }
+            btnNext.isEnabled = amountsUnder50C != collectionViews.count
+        }
+
+        currentCollect.amountLabel.textColor = Decimal(string: (currentCollect.amountLabel.text!.replacingOccurrences(of: ",", with: ".")))! > Decimal(amountLimit) ? UIColor.init(rgb: 0xb91a24).withAlphaComponent(0.5) : UIColor.init(rgb: 0xD2D1D9)
+    }
 //
 //    func showShowcase(message: String, targetView: UIView) {
 //        let showCase = MaterialShowcase()
