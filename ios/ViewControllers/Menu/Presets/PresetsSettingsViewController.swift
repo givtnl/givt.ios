3ab91ca07f75443471cf3c2c50910b1094231830
Mike Pattyn <mike@givtapp.net>
2019-02-21 22:04:58 +0100

    added story board and navigation for presets setting :)

---
+++ ios/ViewControllers/Menu/Presets/PresetsSettingsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PresetsViewController.swift
+//  ios
+//
+//  Created by Mike Pattyn on 21/02/2019.
+//  Copyright © 2019 Givt. All rights reserved.
+//
+
+import UIKit
+
+class PresetsSettingsViewController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
