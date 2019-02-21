909701bb7eae7f7ebf6bb034ceabdf363e4d14e5
Mike Pattyn <mike@givtapp.net>
2019-02-21 22:39:11 +0100

    added functionality for switcher

--- ios/ViewControllers/Menu/Presets/PresetsSettingsViewController.swift
+++ ios/ViewControllers/Menu/Presets/PresetsSettingsViewController.swift
@@ -10,11 +10,49 @@ import UIKit
 
 class PresetsSettingsViewController : UIViewController {
     @IBOutlet var backButton: UIBarButtonItem!
-  
+    @IBOutlet var switcher: UISwitch!
     @IBOutlet var navBar: UINavigationItem!
+    @IBOutlet var gotoPresetsView: UIView!
+    
+    /** ToDo: remember if presets are set  */
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         backButton.accessibilityLabel = NSLocalizedString("Back", comment: "")
         navBar.title = NSLocalizedString("AmountPresetsTitle", comment: "")
+        let tap = UITapGestureRecognizer()
+        tap.addTarget(self, action: #selector(gotoPresets))
+        gotoPresetsView.isUserInteractionEnabled = false
+        gotoPresetsView.addGestureRecognizer(tap)
+        gotoPresetsView.isOpaque = true
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        switcher.isOn = false
+        switcher.isUserInteractionEnabled = true
+        
+        gotoPresetsView.isUserInteractionEnabled = switcher.isOn
+        for childView in gotoPresetsView.subviews {
+            childView.alpha = gotoPresetsView.isUserInteractionEnabled ? 1.0 : 0.4
+        }
+    }
+    
+    @objc func gotoPresets() {
+        let vc = UIStoryboard(name: "AmountPresets", bundle: nil).instantiateViewController(withIdentifier: "AmountPresetsViewController") as! AmountPresetsViewController
+        self.show(vc, sender: self)
+    }
+    @IBAction func `switch`(_ sender: Any) {
+        let sw = sender as! UISwitch
+        if sw.isOn {
+            sw.isUserInteractionEnabled = false
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.35, execute: {
+                self.gotoPresets()
+            })
+        } else {
+            for childView in gotoPresetsView.subviews {
+                gotoPresetsView.isUserInteractionEnabled = switcher.isOn
+                childView.alpha = gotoPresetsView.isUserInteractionEnabled ? 1.0 : 0.4
+            }
+        }
     }
 }
