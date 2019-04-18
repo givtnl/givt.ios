7bece224eb257da1d8ae1c4bc69484d35b902771
Mike Pattyn <mike@givtapp.net>
2019-04-18 10:23:47 +0200

    Added new givy_flash and change font+size on labels.

--- ios/ViewControllers/Give Flow/CelebrationQueueViewController.swift
+++ ios/ViewControllers/Give Flow/CelebrationQueueViewController.swift
@@ -10,19 +10,27 @@ import Foundation
 import UIKit
 
 class CelebrationQueueViewController : UIViewController {
+    
     @IBOutlet var titelLabel: UILabel!
     @IBOutlet var secondaryTitelLabel: UILabel!
-    @IBOutlet var imageView: UIImageView!
+    
+    @IBOutlet var buttonEnablePushNot: CustomButton!
+    @IBOutlet var buttonCancelPartyGivt: CustomButton!
     
     override func viewDidLoad() {
         navigationItem.title = "Queue titel"
-        titelLabel.text = "Titel Label Queue"
-        secondaryTitelLabel.text = "Secondary Titel Label Queue"
-        imageView.image = #imageLiteral(resourceName: "givy_flashlight_frame")
+        titelLabel.text = NSLocalizedString("CelebrationHappyToSeeYou", comment: "")
+        secondaryTitelLabel.text = NSLocalizedString("CelebrationQueueText", comment: "")
+        buttonEnablePushNot.setTitle(NSLocalizedString("CelebrationEnablePushNotification", comment: ""), for: UIControlState.normal)
+        let cancelString = NSLocalizedString("CelebrationQueueCancel", comment: "")
+        let cancelStringMutable = NSMutableAttributedString(string: cancelString)
+        cancelStringMutable.addAttribute(NSAttributedString.Key.underlineStyle, value: NSUnderlineStyle.styleSingle.rawValue, range: NSRange(location: 0, length: cancelString.count))
+        buttonCancelPartyGivt.setAttributedTitle(cancelStringMutable, for: UIControlState.normal)
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationItem.titleView = UIImageView(image: UIImage(named: "pg_give_fourth"))
+        navigationItem.hidesBackButton = true
         navigationController?.navigationBar.backgroundColor = UIColor.white
         navigationController?.navigationBar.isTranslucent = true
         self.sideMenuController?.isLeftViewSwipeGestureEnabled = false
