ff6339daa6dfa81f36cda732cbcb288c4182264e
Bjorn Derudder <bjorn.derudder@gmail.com>
2019-06-05 16:17:22 +0200

    implement possibility to add the timeout to request

--- ios/APIClients/Interfaces/IAPIClient.swift
+++ ios/APIClients/Interfaces/IAPIClient.swift
@@ -10,11 +10,17 @@ import Foundation
 import SwiftClient
 
 protocol IAPIClient {
-    func get(url: String, data: [String: String], headers: [String: String], callback: @escaping (Response?) -> Void) -> Void
+    func get(url: String, data: [String: String], headers: [String: String], timeout: Double, callback: @escaping (Response?) -> Void) -> Void
     
     func put(url: String, data: [String: Any], callback: @escaping (Response?) -> Void) throws -> Void
     
     func post(url: String, data: [String: Any], callback: @escaping (Response?) -> Void) throws -> Void
     
     func post(url: String, data: Data, callback: @escaping (Response?) -> Void) throws -> Void
+}
+
+extension IAPIClient { //extension for the default timeout
+    func get(url: String, data: [String: String], headers: [String: String], timeout: Double = 60, callback: @escaping (Response?) -> Void) -> Void {
+        return get(url: url, data: data, headers: headers, timeout: timeout, callback: callback)
+    }
 }
