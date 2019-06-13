cb6177440366c10b4ca4b1518e8c78db2e5d4666
Jonas Brabant <jonas@givtapp.net>
2019-06-13 09:57:14 +0200

    Create Gift Aid Screens + temp design

---
+++ ios/ViewControllers/Menu/GiftAidInfoController.swift
@@ -0,0 +1,14 @@
+//
+//  GiftAidInfoController.swift
+//  ios
+//
+//  Created by Jonas Brabant on 13/06/2019.
+//  Copyright © 2019 Givt. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class GiftAidInfoController: UIViewController {
+    
+}
