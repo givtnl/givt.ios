3a333bdfd3d1edb350729dfe7dc2039f1e833aef
Jonas Brabant <jonas@givtapp.net>
2019-06-14 12:26:51 +0200

    Completed Gift Aid story (open only when bacs, open in flow when completing registration, ...)

--- ios/ViewControllers/Menu/GiftAidViewController.swift
+++ ios/ViewControllers/Menu/GiftAidViewController.swift
@@ -14,6 +14,8 @@ class GiftAidViewController: UIViewController {
     
     var uExt: LMUserExt?
     private let loginManager = LoginManager.shared
+    var comingFromRegistration = false
+        private var log: LogService = LogService.shared
     
     @IBOutlet weak var giftAidSwitch: UISwitch!
     @IBOutlet weak var lblSettings: UILabel!
@@ -21,7 +23,9 @@ class GiftAidViewController: UIViewController {
     @IBOutlet weak var lblHeaderDisclaimer: UILabel!
     @IBOutlet weak var lblBodyDisclaimer: UILabel!
     @IBOutlet weak var btnSave: CustomButton!
-
+    @IBOutlet weak var btnBack: UIBarButtonItem!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     private var helpViewController = UIStoryboard(name: "Personal", bundle: nil).instantiateViewController(withIdentifier: "GiftAidInfoController") as! GiftAidInfoController
     
     @IBAction func readyAction(_ sender: Any) {
@@ -51,8 +55,22 @@ class GiftAidViewController: UIViewController {
         
         lblSettings.text = NSLocalizedString("GiftAid_Setting", comment:"")
         lblInfo.text = NSLocalizedString("GiftAid_Info", comment: "")
-        lblBodyDisclaimer.text = NSLocalizedString("GiftAid_HeaderDisclaimer", comment:"")
-        lblHeaderDisclaimer.text = NSLocalizedString("GiftAid_BodyDisclaimer", comment: "")
+        lblHeaderDisclaimer.text = NSLocalizedString("GiftAid_HeaderDisclaimer", comment:"")
+        lblBodyDisclaimer.text = NSLocalizedString("GiftAid_BodyDisclaimer", comment: "")
+        
+        if comingFromRegistration {
+            self.btnBack.isEnabled = false;
+            self.btnBack.tintColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0)
+            self.btnBack.image = UIImage()
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        let calcHeight = scrollView.subviews[0].subviews[0].frame.size.height +
+                         scrollView.subviews[0].subviews[1].frame.size.height +
+                         scrollView.subviews[0].subviews[2].frame.size.height + 50
+        scrollView.contentSize =  CGSize(width: scrollView.subviews[0].frame.size.width,
+                                         height: calcHeight)
     }
     
     @IBAction func saveAction(_ sender: Any) {
@@ -78,6 +96,52 @@ class GiftAidViewController: UIViewController {
                                 
                             }))
                             self.present(alert, animated: true, completion: nil)
+                        }
+                    }
+                })
+            }
+        }
+        if comingFromRegistration {
+            if NavigationManager.shared.hasInternetConnection(context: self) {
+                SVProgressHUD.show()
+                LoginManager.shared.requestMandateUrl(completionHandler: { (response) in
+                    SVProgressHUD.dismiss()
+                    if let r = response {
+                        if(r.status == .ok){
+                            LoginManager.shared.finishMandateSigning(completionHandler: { (done) in
+                                print(done)
+                            })
+                            DispatchQueue.main.async {
+                                let vc = UIStoryboard(name: "Registration", bundle: nil).instantiateViewController(withIdentifier: "FinalRegistrationViewController") as! FinalRegistrationViewController
+                                self.navigationController!.pushViewController(vc, animated: true)
+                            }
+                        } else {
+                            let alert = UIAlertController(title: NSLocalizedString("RequestFailed", comment: ""), message: NSLocalizedString("RequestMandateFailed", comment: ""), preferredStyle: UIAlertControllerStyle.alert)
+                            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: { (actions) in
+                                DispatchQueue.main.async {
+                                    self.dismiss(animated: true, completion: nil)
+                                    NavigationManager.shared.loadMainPage(animated: false)
+                                }
+                            }))
+                            if let data = r.data {
+                                do {
+                                    let parsedData = try JSONSerialization.jsonObject(with: data) as! [String: Any]
+                                    if let parsedCode = parsedData["Code"] as? Int {
+                                        if(parsedCode == 111){
+                                            alert.title = NSLocalizedString("DDIFailedTitle", comment: "")
+                                            alert.message = NSLocalizedString("UpdateBacsAccountDetailsError", comment: "")
+                                        } else if (parsedCode == 112){
+                                            alert.title = NSLocalizedString("DDIFailedTitle", comment: "")
+                                            alert.message = NSLocalizedString("DDIFailedMessage", comment: "")
+                                        }
+                                    }
+                                } catch {
+                                    self.log.error(message: "Could not parse givtStatusCode Json probably not valid.")
+                                }
+                            }
+                            DispatchQueue.main.async {
+                                self.present(alert, animated: true, completion: nil)
+                            }
                         }
                     }
                 })
