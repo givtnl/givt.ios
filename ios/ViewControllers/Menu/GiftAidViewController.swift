0307c9ed3f85928ca06fd2fc399834aa84fa1346
Jonas Brabant <jonas@givtapp.net>
2019-06-19 15:59:08 +0200

    Gift Aid - show message when pushing save (on registration), show message when pressing back button

--- ios/ViewControllers/Menu/GiftAidViewController.swift
+++ ios/ViewControllers/Menu/GiftAidViewController.swift
@@ -13,6 +13,7 @@ import UIKit
 class GiftAidViewController: UIViewController {
     
     var uExt: LMUserExt?
+    private var previousStateGiftAid: Bool?
     private let loginManager = LoginManager.shared
     var comingFromRegistration: Bool = false
         private var log: LogService = LogService.shared
@@ -26,6 +27,43 @@ class GiftAidViewController: UIViewController {
     @IBOutlet weak var scrollView: UIScrollView!
     
     private var helpViewController = UIStoryboard(name: "Personal", bundle: nil).instantiateViewController(withIdentifier: "GiftAidInfoController") as! GiftAidInfoController
+    
+    override func viewDidLoad() {
+        giftAidSwitch.setOn(uExt?.GiftAid != nil, animated: false)
+        previousStateGiftAid = giftAidSwitch.isOn
+        
+        lblSettings.text = NSLocalizedString("GiftAid_Setting", comment:"")
+        lblInfo.text = NSLocalizedString("GiftAid_Info", comment: "")
+        lblHeaderDisclaimer.text = NSLocalizedString("GiftAid_HeaderDisclaimer", comment:"")
+        lblBodyDisclaimer.text = NSLocalizedString("GiftAid_BodyDisclaimer", comment: "")
+        
+           if(comingFromRegistration){
+                navigationItem.leftBarButtonItem?.isEnabled = false
+                navigationItem.leftBarButtonItem?.image = nil
+           }
+        navigationItem.leftBarButtonItem?.action = #selector(showAlertPersonalInfo)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        let calcHeight = scrollView.subviews[0].subviews[0].frame.size.height +
+                         scrollView.subviews[0].subviews[1].frame.size.height +
+                         scrollView.subviews[0].subviews[2].frame.size.height + 50
+        scrollView.contentSize =  CGSize(width: scrollView.subviews[0].frame.size.width,
+                                         height: calcHeight)
+    }
+    
+    @objc func showAlertPersonalInfo() {
+        if previousStateGiftAid != giftAidSwitch.isOn {
+            showAlert(title: NSLocalizedString("ImportantMessage", comment: ""),
+                      message: NSLocalizedString("GiftAidUnsavedChanges", comment: ""),
+                      action1: UIAlertAction(title: NSLocalizedString("Dismiss", comment: ""), style: .default, handler: { action in
+                        self.navigationController?.popViewController(animated: true)
+                      }),
+                      action2: UIAlertAction(title: NSLocalizedString("Back", comment: ""), style: .default, handler: { action in }) )
+        } else {
+            self.navigationController?.popViewController(animated: true)
+        }
+    }
     
     @IBAction func readyAction(_ sender: Any) {
         LoginManager.shared.updateUser(uext: self.uExt!, completionHandler: { (ok) in
@@ -44,31 +82,9 @@ class GiftAidViewController: UIViewController {
     }
     
     @IBAction func openInfo(_ sender: Any) {
-                helpViewController.title = NSLocalizedString("GiftAidInfo_Title", comment: "")
-                helpViewController.bodyText = NSLocalizedString("GiftAidInfo_Body", comment: "")
-                self.present(helpViewController, animated: true, completion: nil)
-    }
-    
-    override func viewDidLoad() {
-        giftAidSwitch.setOn(uExt?.GiftAid != nil, animated: false)
-        
-        lblSettings.text = NSLocalizedString("GiftAid_Setting", comment:"")
-        lblInfo.text = NSLocalizedString("GiftAid_Info", comment: "")
-        lblHeaderDisclaimer.text = NSLocalizedString("GiftAid_HeaderDisclaimer", comment:"")
-        lblBodyDisclaimer.text = NSLocalizedString("GiftAid_BodyDisclaimer", comment: "")
-        
-           if(comingFromRegistration){
-                navigationItem.leftBarButtonItem?.isEnabled = false
-                navigationItem.leftBarButtonItem?.image = nil
-           }
-    }
-    
-    override func viewDidLayoutSubviews() {
-        let calcHeight = scrollView.subviews[0].subviews[0].frame.size.height +
-                         scrollView.subviews[0].subviews[1].frame.size.height +
-                         scrollView.subviews[0].subviews[2].frame.size.height + 50
-        scrollView.contentSize =  CGSize(width: scrollView.subviews[0].frame.size.width,
-                                         height: calcHeight)
+        helpViewController.title = NSLocalizedString("GiftAidInfo_Title", comment: "")
+        helpViewController.bodyText = NSLocalizedString("GiftAidInfo_Body", comment: "")
+        self.present(helpViewController, animated: true, completion: nil)
     }
     
     @IBAction func saveAction(_ sender: Any) {
@@ -85,27 +101,41 @@ class GiftAidViewController: UIViewController {
                     DispatchQueue.main.async {
                         if(self.comingFromRegistration){
                             let vc = UIStoryboard(name: "Registration", bundle: nil).instantiateViewController(withIdentifier: "FinalRegistrationViewController") as! FinalRegistrationViewController
-                            self.navigationController!.pushViewController(vc, animated: true)
                             
-                            
+                                self.showAlert(title: NSLocalizedString("ImportantMessage", comment: ""),
+                                               message: NSLocalizedString("GiftAidChangeLater", comment: ""),
+                                               action1: UIAlertAction(title: NSLocalizedString("GotIt", comment: ""), style: .default, handler: { action in
+                                                
+                                                self.navigationController!.pushViewController(vc, animated: true)
+                                               }),
+                                               action2: nil )
                         } else {
                             self.navigationController?.popViewController(animated: true)
-                            
                         }
                     }
+                    
                 } else {
-                    self.showSaveFailedError()
+                    DispatchQueue.main.async {
+                        let alert = UIAlertController(title: NSLocalizedString("SaveFailed", comment: ""), message: NSLocalizedString("UpdatePersonalInfoError", comment: ""), preferredStyle: UIAlertControllerStyle.alert)
+                        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: { action in
+                            
+                        }))
+                        self.present(alert, animated: true, completion: nil)
+                    }
                 }
             })
         }
     }
-    private func showSaveFailedError() {
-        DispatchQueue.main.async {
-            let alert = UIAlertController(title: NSLocalizedString("SaveFailed", comment: ""), message: NSLocalizedString("UpdatePersonalInfoError", comment: ""), preferredStyle: UIAlertControllerStyle.alert)
-            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: { action in
-                
-            }))
-            self.present(alert, animated: true, completion: nil)
-        }
+    
+    func showAlert(title:String, message:String, action1: UIAlertAction, action2: UIAlertAction?) {
+            let alert = UIAlertController(
+                title: NSLocalizedString(title, comment: ""),
+                message: NSLocalizedString(message, comment: ""),
+                preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(action1)
+            if let _action2 = action2 {
+                    alert.addAction(_action2)
+            }
+            present(alert, animated: true, completion: nil)
     }
 }
