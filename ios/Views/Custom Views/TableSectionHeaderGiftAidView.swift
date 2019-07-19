aeeb9083a725a5913de0844084cf2053b81b0fca
Jonas Brabant <jonas@givtapp.net>
2019-07-19 10:23:53 +0200

    Added TableSectionHeaderGiftAidView

---
+++ ios/Views/Custom Views/TableSectionHeaderGiftAidView.swift
@@ -0,0 +1,14 @@
+//
+//  TableSectionHeaderGiftAidView.swift
+//  ios
+//
+//  Created by Jonas Brabant on 18/07/2019.
+//  Copyright © 2019 Givt. All rights reserved.
+//
+
+import UIKit
+
+class TableSectionHeaderGiftAidView: UITableViewHeaderFooterView {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var amountLabel: UILabel!
+}
