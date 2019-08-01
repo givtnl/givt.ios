449e4b02552cff3cdec8b8725f5e69e196075301
Mike Pattyn <mike@givtapp.net>
2019-08-01 11:48:20 +0200

    forgot to remove this param

--- ios/Managers/LoginManager.swift
+++ ios/Managers/LoginManager.swift
@@ -349,7 +349,7 @@ class LoginManager {
     }
     
     func registerEmailOnly(email: String, completionHandler: @escaping (Bool) -> Void) {
-        let regUser = RegistrationUser(email: email, password: AppConstants.tempUserPassword, firstName: "John", lastName: "Doe", address: "Foobarstraat 5", city: "Foobar", country: "NL", iban: AppConstants.tempIban, mobileNumber: "0600000000", postalCode: "786 FB", sortCode: "", bacsAccountNumber: "", amountLimit: 499)
+        let regUser = RegistrationUser(email: email, password: AppConstants.tempUserPassword, firstName: "John", lastName: "Doe", address: "Foobarstraat 5", city: "Foobar", country: "NL", iban: AppConstants.tempIban, mobileNumber: "0600000000", postalCode: "786 FB", sortCode: "", bacsAccountNumber: "")
         
         if let countryCode = AppServices.getCountryFromSim() {
             regUser.country = countryCode
