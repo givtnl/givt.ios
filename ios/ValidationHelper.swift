210a55221a2aef83e1e52925f0f9dbf33f3282be
Bjorn Derudder <bjorn.derudder@gmail.com>
2019-10-07 16:19:29 +0200

    Fix underscore in mail

--- ios/ValidationHelper.swift
+++ ios/ValidationHelper.swift
@@ -29,7 +29,7 @@ class ValidationHelper {
     }
     
     func isEmailAddressValid(_ string: String) -> Bool {
-        let firstpart = "[A-Z0-9a-z]([A-Z0-9a-z._%+-]{0,30}[A-Z0-9a-z])?"
+        let firstpart = "[A-Z0-9a-z]([A-Z0-9a-z._%+-]{0,30})?"
         let serverpart = "([A-Z0-9a-z]([A-Z0-9a-z-]{0,30}[A-Z0-9a-z])?\\.){1,5}"
         let emailRegex = firstpart + "@" + serverpart + "[A-Za-z]{2,6}"
         let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
