cf981c5162a3c5cab22fa727c723e67209f2ef86
Mike <mike@givtapp.net>
2020-07-27 12:05:50 +0200

    add storyboard, choose amount vc, choose destination vc, a few routes

---
+++ ios/Stories/SetupRecurringDonation/Scenes/ChooseDestination/Models/DestinationTableCell.swift
@@ -0,0 +1,89 @@
+//
+//  DestinationTableCell.swift
+//  ios
+//
+//  Created by Mike Pattyn on 27/07/2020.
+//  Copyright © 2020 Givt. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+internal final class DestinationTableCell : UITableViewCell {
+    public var name: String = "" {
+        didSet {
+            collectGroupLabel.text = name
+            collectGroupLabel.numberOfLines = 0
+        }
+    }
+    
+    public var type: CollectGroupType = .church {
+        didSet {
+            switch type {
+            case .church:
+                iconLabel.text = "place-of-worship"
+                break
+            case .charity:
+                iconLabel.text = "hands-helping"
+                break
+            case .campaign:
+                iconLabel.text = "hand-holding-heart"
+                break
+            case .artist:
+                iconLabel.text = "guitar"
+                break
+            default:
+                iconLabel.text = "place-of-worship"
+                break
+            }
+        }
+    }
+
+    @IBOutlet var iconLabel: UILabel!
+    @IBOutlet var collectGroupLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        selectionStyle = .none
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    func toggleOff() {
+        setBackgroundColorRecursive(view: self.contentView, color: UIColor.clear)
+        iconLabel.textColor = #colorLiteral(red: 0.1803921569, green: 0.1607843137, blue: 0.3411764706, alpha: 1)
+        collectGroupLabel.textColor = #colorLiteral(red: 0.1803921569, green: 0.1607843137, blue: 0.3411764706, alpha: 1)
+    }
+    
+    func toggleOn() {
+        switch type {
+        case .church:
+            setBackgroundColorRecursive(view: self.contentView, color: #colorLiteral(red: 0.09952672571, green: 0.41830042, blue: 0.7092369199, alpha: 1))
+            break
+        case .charity:
+            setBackgroundColorRecursive(view: self.contentView, color: #colorLiteral(red: 1, green: 0.6917269826, blue: 0, alpha: 1))
+            break
+        case .campaign:
+            setBackgroundColorRecursive(view: self.contentView, color: #colorLiteral(red: 0.9460871816, green: 0.4409908056, blue: 0.3430213332, alpha: 1))
+            break
+        case .artist:
+            setBackgroundColorRecursive(view: self.contentView, color: #colorLiteral(red: 0.2549019608, green: 0.7882352941, blue: 0.5568627451, alpha: 1))
+            break
+        default:
+            setBackgroundColorRecursive(view: self.contentView, color: #colorLiteral(red: 0.09952672571, green: 0.41830042, blue: 0.7092369199, alpha: 1))
+            break
+        }
+        iconLabel.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        collectGroupLabel.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    }
+    
+    private func setBackgroundColorRecursive(view: UIView, color: UIColor) {
+        view.backgroundColor = color
+        view.subviews.forEach {
+            setBackgroundColorRecursive(view: $0, color: color)
+        }
+    }
+}
+
---
+++ ios/Stories/SetupRecurringDonation/Scenes/ChooseDestination/Models/DestinationViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  DestinationViewModel.swift
+//  ios
+//
+//  Created by Mike Pattyn on 27/07/2020.
+//  Copyright © 2020 Givt. All rights reserved.
+//
+
+import Foundation
+
+internal class DestinationViewModel {
+    public var name = String()
+    public var type = CollectGroupType.church
+    public var selected: Bool!
+}
