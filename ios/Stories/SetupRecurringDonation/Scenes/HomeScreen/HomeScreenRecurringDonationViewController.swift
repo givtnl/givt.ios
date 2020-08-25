380ac97af8637bbce04e646c7fb350e5923952c2
Jonas Brabant <jonas@givtapp.net>
2020-08-25 17:10:00 +0200

    create homescreen

---
+++ ios/Stories/SetupRecurringDonation/Scenes/HomeScreen/HomeScreenRecurringDonationViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HomeScreenRecurringDonationViewController.swift
+//  ios
+//
+//  Created by Jonas Brabant on 25/08/2020.
+//  Copyright © 2020 Givt. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class HomeScreenRecurringDonationViewController: UIViewController
+{
+
+}
