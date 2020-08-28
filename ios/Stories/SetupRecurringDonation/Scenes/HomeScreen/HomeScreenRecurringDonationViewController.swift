6347d45377c5deef02dbb29847404af08624c020
Jonas Brabant <jonas@givtapp.net>
2020-08-28 15:06:48 +0200

    Make up basic table cell creation + models

--- ios/Stories/SetupRecurringDonation/Scenes/HomeScreen/HomeScreenRecurringDonationViewController.swift
+++ ios/Stories/SetupRecurringDonation/Scenes/HomeScreen/HomeScreenRecurringDonationViewController.swift
@@ -9,9 +9,36 @@
 import UIKit
 import Foundation
 
-class HomeScreenRecurringDonationViewController: UIViewController
+class HomeScreenRecurringDonationViewController: UIViewController,  UITableViewDelegate, UITableViewDataSource
 {
+    @IBOutlet weak var tableView: UITableView!
+    
     private var mediater: MediaterWithContextProtocol = Mediater.shared
+    
+    // Data model
+    let animals: [String] = ["Horse", "Cow", "Camel", "Sheep", "Goat"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        self.tableView.tableFooterView = UIView()
+    }
+    
+    // number of rows in table view
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.animals.count
+    }
+    
+    // create a cell for each table view row
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RecurringRuleTableCell.self), for: indexPath) as! RecurringRuleTableCell
+        
+        cell.Name.text = self.animals[indexPath.row]
+        return cell
+    }
     
     @IBAction func createRecurringDonationButtonTapped(_ sender: Any) {
         try? mediater.send(request: GoToChooseSubscriptionRoute(), withContext: self)
@@ -20,4 +47,6 @@ class HomeScreenRecurringDonationViewController: UIViewController
     @IBAction func backButton(_ sender: Any) {
         try? mediater.send(request: BackToMainRoute(), withContext: self)
     }
+    
+    
 }
