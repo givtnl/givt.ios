ad297ae6e127a6c852ce7ae8a73f844ec0a5a9f6
Jonas Brabant <jonas@givtapp.net>
2020-09-16 11:57:09 +0200

    Setup basic layout principles

---
+++ ios/Stories/SetupRecurringDonation/Scenes/SetupNofication/SetupNotificationRecurringDonationViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SetupNotificationRecurringDonationViewController.swift
+//  ios
+//
+//  Created by Jonas Brabant on 16/09/2020.
+//  Copyright © 2020 Givt. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SetupNotificationRecurringDonationViewController: UIViewController
+{
+    
+}
