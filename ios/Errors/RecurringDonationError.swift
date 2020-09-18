5dc1115e4eac49670f68b13f18a744868c7fed97
Mike <mike@givtapp.net>
2020-09-18 09:36:33 +0200

    refactor create recurring donation even more

---
+++ ios/Errors/RecurringDonationError.swift
@@ -0,0 +1,13 @@
+//
+//  RecurringDonationError.swift
+//  ios
+//
+//  Created by Mike Pattyn on 18/09/2020.
+//  Copyright © 2020 Givt. All rights reserved.
+//
+
+import Foundation
+
+enum RecurringDonationError: Error {
+    case wrongDate
+}
--- ios/Stories/SetupRecurringDonation/Scenes/ChooseRecurringDonation/SetupRecurringDonationChooseRecurringDonationViewController.swift
+++ ios/Stories/SetupRecurringDonation/Scenes/ChooseRecurringDonation/SetupRecurringDonationChooseRecurringDonationViewController.swift
@@ -109,121 +109,48 @@ class SetupRecurringDonationChooseRecurringDonationViewController: UIViewControl
         MSAnalytics.trackEvent("RECURRING_DONATIONS_CREATION_GIVE_CLICKED")
         Mixpanel.mainInstance().track(event: "RECURRING_DONATIONS_CREATION_GIVE_CLICKED")
         
-        
-        let cronExpression: String
-        
-        let dayOfMonth = startDatePicker.date.getDay()
-        let month = startDatePicker.date.getMonth()
-        
-        switch frequencys[frequencyPicker.selectedRow(inComponent: 0)][0] as! Frequency {
-        case Frequency.Weekly:
-            let dayOfWeek: String
-            let myCalendar = Calendar(identifier: .gregorian)
-            switch myCalendar.component(.weekday, from: startDatePicker.date) {
-            case 0, 7:
-                dayOfWeek = "SAT"
-            case 1:
-                dayOfWeek = "SUN"
-            case 2:
-                dayOfWeek = "MON"
-            case 3:
-                dayOfWeek = "TUE"
-            case 4:
-                dayOfWeek = "WED"
-            case 5:
-                dayOfWeek = "THU"
-            case 6:
-                dayOfWeek = "FRI"
-            default:
-                dayOfWeek = "SUN"
-            }
-            cronExpression = "0 0 * * \(dayOfWeek)"
-        case Frequency.Monthly:
-            cronExpression = "0 0 \(dayOfMonth) * *"
-        case Frequency.ThreeMonthly:
-            cronExpression = "0 0 \(dayOfMonth) \(getFirstPartQuarterlyCronMonth(month: month))/3 *"
-        case Frequency.SixMonthly:
-            cronExpression = "0 0 \(dayOfMonth) \(getFirstPartHalfYearlyCronMonth(month: month))/6 *"
-        case Frequency.Yearly:
-            cronExpression = "0 0 \(dayOfMonth) \(month+1)/12 *"
+        if !AppServices.shared.isServerReachable {
+            try? mediater.send(request: NoInternetAlert(), withContext: self)
+            return
         }
         
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd"
-        let startDeet: String = dateFormatter.string(from: startDatePicker.date)
+        SVProgressHUD.show()
         
-        if AppServices.shared.isServerReachable {
-            SVProgressHUD.show()
-            
-            guard let countryString: String = try? mediater.send(request: GetCountryQuery()) else {
+        guard let countryString: String = try? mediater.send(request: GetCountryQuery()) else {
+            self.showSetupRecurringDonationFailed()
+            return
+        }
+        
+        guard let mediumIdString: String = self.input?.mediumId else {
+            self.showSetupRecurringDonationFailed()
+            return
+        }
+        
+        guard let occurencesInteger: Int = Int(self.occurrencesTextField.text!) else {
+            self.showSetupRecurringDonationFailed()
+            return
+        }
+        
+        let command = CreateRecurringDonationCommand(amountPerTurn: self.amountView.amount, namespace: mediumIdString, endsAfterTurns: occurencesInteger, startDate: startDatePicker.date.toString("yyyy-MM-dd"), country: countryString, frequency: frequencys[frequencyPicker.selectedRow(inComponent: 0)][0] as! Frequency)
+        
+        NavigationManager.shared.executeWithLogin(context: self) {
+            if !LoginManager.shared.isUserLoggedIn {
                 self.showSetupRecurringDonationFailed()
-                return
-            }
-            
-            guard let mediumIdString: String = self.input?.mediumId else {
-                self.showSetupRecurringDonationFailed()
-                return
-            }
-            
-            guard let occurencesInteger: Int = Int(self.occurrencesTextField.text!) else {
-                self.showSetupRecurringDonationFailed()
-                return
-            }
-            
-            let command = CreateRecurringDonationCommand(amountPerTurn: self.amountView.amount, namespace: mediumIdString, endsAfterTurns: occurencesInteger, cronExpression: cronExpression, startDate: startDeet, country: countryString)
-            
-            NavigationManager.shared.executeWithLogin(context: self) {
-                if !LoginManager.shared.isUserLoggedIn {
-                    SVProgressHUD.dismiss()
-                    self.showSetupRecurringDonationFailed()
-                } else {
-                    do {
-                        try self.mediater.sendAsync(request: command) { recurringDonationMade in
-                            if(recurringDonationMade) {
-                                DispatchQueue.main.async {
-                                    try? self.mediater.send(request: PopToRecurringDonationOverviewRoute(), withContext: self)
-                                }
-                            } else {
-                                SVProgressHUD.dismiss()
-                                DispatchQueue.main.async {
-                                    let alert = UIAlertController(title: "SomethingWentWrong".localized, message: "SetupRecurringDonationFailed".localized, preferredStyle: .alert)
-                                    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action) in
-                                    }))
-                                    self.present(alert, animated: true, completion:  {})
-                                }
-                            }
+            } else {
+                do {
+                    try self.mediater.sendAsync(request: command) { recurringDonationMade in
+                        if !recurringDonationMade {
+                            self.showSetupRecurringDonationFailed()
+                            return
                         }
-                    } catch {
-                        self.showSetupRecurringDonationFailed()
+                        DispatchQueue.main.async {
+                            try? self.mediater.send(request: PopToRecurringDonationOverviewRoute(), withContext: self)
+                        }
                     }
+                } catch {
+                    self.showSetupRecurringDonationFailed()
                 }
             }
-        } else {
-            try? mediater.send(request: NoInternetAlert(), withContext: self)
-        }
-    }
-    
-    private func getFirstPartQuarterlyCronMonth(month: Int) -> Int {
-        switch month {
-        case 0,3,6,9:
-            return 1
-        case 1,4,7,10:
-            return 2
-        case 2,5,8,11:
-            return 3
-        default:
-            return 0
-        }
-    }
-    
-    private func getFirstPartHalfYearlyCronMonth(month: Int) -> Int {
-        switch month {
-        case 0,1,2,3,4,5:
-            return month
-        case 6,7,8,9,10,11:
-            return month - 6
-        default:
-            return 0
         }
     }
 }
@@ -231,7 +158,7 @@ class SetupRecurringDonationChooseRecurringDonationViewController: UIViewControl
 extension SetupRecurringDonationChooseRecurringDonationViewController : CollectGroupLabelDelegate {
     private func showSetupRecurringDonationFailed() {
         SVProgressHUD.dismiss()
-
+        
         DispatchQueue.main.async {
             let alert = UIAlertController(title: "SomethingWentWrong".localized, message: "SetupRecurringDonationFailed".localized, preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action) in
@@ -412,14 +339,6 @@ extension SetupRecurringDonationChooseRecurringDonationViewController : CollectG
         Mixpanel.mainInstance().track(event: "RECURRING_DONATIONS_CREATION_FREQUENCY_CHANGED", properties: ["frequency": frequencys[row][1] as! String])
         pickerView.reloadAllComponents()
         ensureButtonHasCorrectState()
-    }
-    
-    private enum Frequency {
-        case Weekly
-        case Monthly
-        case ThreeMonthly
-        case SixMonthly
-        case Yearly
     }
     
     func createToolbar(_ textField: UITextField) {
