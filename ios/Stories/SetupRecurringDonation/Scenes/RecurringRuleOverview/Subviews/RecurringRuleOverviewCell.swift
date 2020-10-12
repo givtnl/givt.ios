47676bdb755de3d4969f6ebaa291cd19d0fe979b
Jonas Brabant <jonas@givtapp.net>
2020-10-12 15:13:28 +0200

    Setup a recurring rule overview cell

---
+++ ios/Stories/SetupRecurringDonation/Scenes/RecurringRuleOverview/Subviews/RecurringRuleOverviewCell.swift
@@ -0,0 +1,16 @@
+//
+//  RecurringRuleOverviewCell.swift
+//  ios
+//
+//  Created by Jonas Brabant on 12/10/2020.
+//  Copyright © 2020 Givt. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+@IBDesignable
+internal final class RecurringRuleOverviewCell : UITableViewCell {
+    
+   
+}
