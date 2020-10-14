e6019be2a0c0990a561e1992845b2368d381225f
Jonas Brabant <jonas@givtapp.net>
2020-10-14 11:00:07 +0200

    Nasty commit! - current status

--- ios/Stories/SetupRecurringDonation/Scenes/RecurringRuleOverview/RecurringRuleOverviewRecurringDonationViewController.swift
+++ ios/Stories/SetupRecurringDonation/Scenes/RecurringRuleOverview/RecurringRuleOverviewRecurringDonationViewController.swift
@@ -7,29 +7,44 @@
 //
 
 import Foundation
+import SVProgressHUD
 import UIKit
 
 class RecurringRuleOverviewRecurringDonationViewController : UIViewController
 {
+    var recurringDonationId: String?
+    var donations: [RecurringDonationDonationViewModel] = []
+    public var reloadData: Bool = true;
     private var mediater: MediaterWithContextProtocol = Mediater.shared
     
-    var recurringDonationId: String?
-    var donations: [RecurringDonationDonationViewModel]?
-    
+    @IBOutlet var navBar: UINavigationItem!
     @IBOutlet var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let nib = UINib(nibName: "RecurringRuleOverviewCell", bundle: nil)
-        tableView.register(nib, forHeaderFooterViewReuseIdentifier: "recurringRuleOverviewCell")
-        
+        navBar.title = "TitleRecurringGifts".localized
+//        tableView.delegate = self
+//        tableView.dataSource = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        tableView.isHidden = true
+        SVProgressHUD.show()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
         do {
             self.donations = try self.mediater.send(request: GetDonationsFromRecurringDonationQuery(id: recurringDonationId!))
         } catch  {
             print("ERROR: GOESTING NOT FOUND")
         }
+        self.tableView.reloadData()
+        SVProgressHUD.dismiss()
         
-        print(self.donations)
+    }
+    
+    @IBAction func backButton(_ sender: Any) {
+        try? mediater.send(request: BackToRecurringDonationOverviewRoute(), withContext: self)
     }
 }
