15c0d464bab9bb15ac98ff5e8c1a6f5212954301
Mike Pattyn <mike@givtapp.net>
2020-10-15 15:23:16 +0200

    move logics to table cell when set viewmodel

--- ios/ColorHelper.swift
+++ ios/ColorHelper.swift
@@ -12,6 +12,7 @@ public class ColorHelper {
     public static var GivtBlue: UIColor =  #colorLiteral(red: 0.1843137255, green: 0.5058823529, blue: 0.7843137255, alpha: 1)
     public static var GivtOrange: UIColor = #colorLiteral(red: 0.9294117647, green: 0.6470588235, blue: 0.1803921569, alpha: 1)
     public static var GivtRed: UIColor =  #colorLiteral(red: 0.8823529412, green: 0.4196078431, blue: 0.3333333333, alpha: 1)
+    public static var GivtLightGreen: UIColor = #colorLiteral(red: 0.2352941176, green: 0.7803921569, blue: 0.5490196078, alpha: 1)
     public static var GivtGreen: UIColor = #colorLiteral(red: 0.1137254902, green: 0.662745098, blue: 0.4235294118, alpha: 1)
     public static var GivtPurple: UIColor = #colorLiteral(red: 0.1803921569, green: 0.1607843137, blue: 0.3411764706, alpha: 1)
     public static var Church: UIColor = #colorLiteral(red: 0.1843137255, green: 0.5058823529, blue: 0.7843137255, alpha: 1)
