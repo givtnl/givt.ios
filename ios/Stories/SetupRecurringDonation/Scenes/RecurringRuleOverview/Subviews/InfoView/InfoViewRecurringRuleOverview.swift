afc396fcd26dea276b7e6aa0e4c05f2347ff8b89
Jonas Brabant <jonas@givtapp.net>
2020-10-20 17:19:15 +0200

    add info recurring rule items

--- ios/Stories/SetupRecurringDonation/Scenes/RecurringRuleOverview/Subviews/InfoView/InfoViewRecurringRuleOverview.swift
+++ ios/Stories/SetupRecurringDonation/Scenes/RecurringRuleOverview/Subviews/InfoView/InfoViewRecurringRuleOverview.swift
@@ -14,6 +14,22 @@ class InfoViewRecurringRuleOverview: UIView {
     @IBOutlet weak var closeInfoView: UIView!
     @IBOutlet weak var closeInfoViewImage: UIImageView!
     @IBOutlet var containerView: UIView!
+    
+    @IBOutlet var infoViewTitle: UILabel!
+    
+    @IBOutlet var elementInProcessTitle: UILabel!
+    @IBOutlet var elementProcessedTitle: UILabel!
+    @IBOutlet var elementRefusedByBankTitle: UILabel!
+    @IBOutlet var elementCancelledByUserTitle: UILabel!
+    @IBOutlet var elementGiftAidedTitle: UILabel!
+    
+    @IBOutlet var elementInProcessStatus: UIView!
+    @IBOutlet var elementProcessedStatus: UIView!
+    @IBOutlet var elementRefusedByBankStatus: UIView!
+    @IBOutlet var elementCancelledByUserStatus: UIView!
+    @IBOutlet var elementGiftAidedStatus: UIImageView!
+    
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         commonInit()
@@ -31,5 +47,25 @@ class InfoViewRecurringRuleOverview: UIView {
         addSubview(contentView)
         contentView.frame = self.bounds
         contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        
+        infoViewTitle.text = "HistoryInfoTitle".localized
+        elementInProcessTitle.text = "HistoryAmountAccepted".localized
+        elementProcessedTitle.text = "HistoryAmountCollected".localized
+        elementRefusedByBankTitle.text = "HistoryAmountDenied".localized
+        elementCancelledByUserTitle.text = "HistoryAmountCancelled".localized
+        elementGiftAidedTitle.text = "Gift Aided"
+        
+        elementInProcessStatus.backgroundColor = UIColor.init(rgb: 0x494874)
+        elementInProcessStatus.layer.cornerRadius = 7.5
+        elementProcessedStatus.backgroundColor = UIColor.init(rgb: 0x41c98e)
+        elementProcessedStatus.layer.cornerRadius = 7.5
+        elementRefusedByBankStatus.backgroundColor = UIColor.init(rgb: 0xd43d4c)
+        elementRefusedByBankStatus.layer.cornerRadius = 7.5
+        elementCancelledByUserStatus.backgroundColor = UIColor.init(rgb: 0xbcb9c9)
+        elementCancelledByUserStatus.layer.cornerRadius = 7.5
+        
+        if UserDefaults.standard.accountType == AccountType.bacs {
+           
+        }
     }
 }
