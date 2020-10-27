097fcf2d136f86db312a130e78baa28f0ba5ec8d
Mike Pattyn <mike@givtapp.net>
2020-10-27 13:43:56 +0100

    create storyboard and viewcontroller for categories

---
+++ ios/Stories/DiscoverOrAmount/Discover/DiscoverViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DiscoverViewController.swift
+//  ios
+//
+//  Created by Mike Pattyn on 27/10/2020.
+//  Copyright © 2020 Givt. All rights reserved.
+//
+
+import UIKit
+
+class DiscoverViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
