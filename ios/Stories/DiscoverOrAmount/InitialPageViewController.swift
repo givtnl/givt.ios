b2041477efc5a1b6e29cc9125cdfbc9334ab6072
Mike Pattyn <mike@givtapp.net>
2020-10-28 13:42:04 +0100

    move functions to extension

--- ios/Stories/DiscoverOrAmount/InitialPageViewController.swift
+++ ios/Stories/DiscoverOrAmount/InitialPageViewController.swift
@@ -9,36 +9,17 @@
 import UIKit
 
 class InitialPageViewController: UIPageViewController {
-    @objc func segmentControlTapped() {
-        if viewControllers?.count == 1 {
-            if viewControllers?[0] is AmountViewController {
-                setViewControllers([items[1]], direction: .forward, animated: true, completion: nil)
-            } else {
-                setViewControllers([items[0]], direction: .reverse, animated: true, completion: nil)
-            }
-        }
-    }
-    
     fileprivate var items: [UIViewController] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         NotificationCenter.default.addObserver(self, selector: #selector(segmentControlTapped), name: .GivtSegmentControlStateDidChange, object: nil)
-
         dataSource = self
 
         populateItems()
         if let firstViewController = items.first {
             setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
         }
-    }
-    
-    fileprivate func populateItems() {
-        let vc1 = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "AmountViewController") as! AmountViewController
-        let vc2 = UIStoryboard(name: "DiscoverOrAmount", bundle: nil).instantiateViewController(withIdentifier: "DiscoverViewController") as! DiscoverViewController
-        items.append(vc1)
-        items.append(vc2)
     }
 }
 extension InitialPageViewController: UIPageViewControllerDataSource, UIPageViewControllerDelegate {
@@ -57,6 +38,19 @@ extension InitialPageViewController: UIPageViewControllerDataSource, UIPageViewC
             return items[0]
         }
     }
-    
-    
+    @objc func segmentControlTapped() {
+        if viewControllers?.count == 1 {
+            if viewControllers?[0] is AmountViewController {
+                setViewControllers([items[1]], direction: .forward, animated: true, completion: nil)
+            } else {
+                setViewControllers([items[0]], direction: .reverse, animated: true, completion: nil)
+            }
+        }
+    }
+    fileprivate func populateItems() {
+        let vc1 = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "AmountViewController") as! AmountViewController
+        let vc2 = UIStoryboard(name: "DiscoverOrAmount", bundle: nil).instantiateViewController(withIdentifier: "DiscoverViewController") as! DiscoverViewController
+        items.append(vc1)
+        items.append(vc2)
+    }
 }
