846b206e07d6dd1969eed83502cbe8c49064aa5b
Maarten Vergouwe <mvergouwe85@gmail.com>
2020-11-19 12:12:10 +0100

    Add Offline success screen to one off donation

---
+++ ios/Stories/DiscoverOrAmount/Routes/OpenOfflineSuccess/DiscoverOrAmountOpenOfflineSuccessRouteHandler.swift
@@ -0,0 +1,25 @@
+//
+//  OpenOfflineSuccessRouteHandler.swift
+//  ios
+//
+//  Created by Maarten Vergouwe on 19/11/2020.
+//  Copyright © 2020 Givt. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DiscoverOrAmountOpenOfflineSuccessRouteHandler : RequestHandlerWithContextProtocol {
+    func handle<R>(request: R, withContext context: UIViewController, completion: @escaping (R.TResponse) throws -> Void) throws where R : RequestProtocol {
+        let request = request as! DiscoverOrAmountOpenOfflineSuccessRoute
+        let vc = UIStoryboard.init(name: "DiscoverOrAmount", bundle: nil)
+            .instantiateViewController(withIdentifier: String(describing: DiscoverOrAmountSuccessViewController.self)) as! DiscoverOrAmountSuccessViewController
+        vc.subtitleText = "OfflineGegevenGivtMessageWithOrg".localized.replacingOccurrences(of: "{0}", with: request.collectGroupName)
+        context.navigationController?.pushViewController(vc, animated: true)
+        try completion(() as! R.TResponse)
+    }
+    
+    func canHandle<R>(request: R) -> Bool where R : RequestProtocol {
+        return request is DiscoverOrAmountOpenOfflineSuccessRoute
+    }
+}
