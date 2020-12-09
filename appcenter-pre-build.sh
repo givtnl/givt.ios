6829b0018a7832bae897ab233a2428585a5c3f6b
Mike Pattyn <mike@givtapp.net>
2020-12-09 13:25:45 +0100

    added some logging to the script

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+echo "Executing prebuild script."
 if [ "$APPCENTER_BRANCH" == "master" ];
     then
+    	echo "Getting collectgroups for prod."
         curl -H "Accept: application/json" https://api.givtapp.net/api/v2/collectgroups/applist > ios/collectGroupsList.json
     else
+    	echo "Getting collectgroups for dev or pre."	
         curl -H "Accept: application/json" https://givt-debug-api.azurewebsites.net/api/v2/collectgroups/applist > ios/collectGroupsListDebug.json
 fi
