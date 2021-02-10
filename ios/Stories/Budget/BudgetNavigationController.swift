d6399631012b572f3a11bd6a6a65d3055107578b
Mike Pattyn <mike.pattyn1@gmail.com>
2021-02-10 11:56:13 +0100

    add menu item and custom views for first 2 elements on screen

---
+++ ios/Stories/Budget/BudgetNavigationController.swift
@@ -0,0 +1,19 @@
+//
+//  BudgetNavigationController.swift
+//  ios
+//
+//  Created by Mike Pattyn on 10/02/2021.
+//  Copyright © 2021 Givt. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class BudgetNavigationController : UINavigationController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationBar.shadowImage = UIImage()
+        self.navigationBar.titleTextAttributes = [ NSAttributedString.Key.font: UIFont(name: "Avenir-Heavy", size: 18)!, NSAttributedString.Key.foregroundColor: #colorLiteral(red: 0.1803921569, green: 0.1607843137, blue: 0.3411764706, alpha: 1)]
+    }
+}
