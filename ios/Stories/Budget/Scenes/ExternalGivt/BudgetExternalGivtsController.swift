42a3344dc22aea423333351c60b181038bd4ac46
Mike Pattyn <mike.pattyn1@gmail.com>
2021-02-27 19:35:56 +0100

    setup second view controller for external givts todo: amount field

--- ios/Stories/Budget/Scenes/ExternalGivt/BudgetExternalGivtsController.swift
+++ ios/Stories/Budget/Scenes/ExternalGivt/BudgetExternalGivtsController.swift
@@ -11,11 +11,21 @@ import SVProgressHUD
 
 class BudgetExternalGivtsViewController : UIViewController {
     @IBOutlet weak var labelExternalGivtsInfo: UILabel!
-    
+    @IBOutlet weak var labelExternalGivtsSubtitle: UILabel!
+    @IBOutlet weak var labelExternalGivtsOrganisation: UILabel!
+    @IBOutlet weak var labelExternalGivtsTime: UILabel!
+    @IBOutlet weak var labelExternalGivtsTimeDown: UILabel!
+    @IBOutlet weak var labelExternalGivtsAmount: UILabel!
+    @IBOutlet weak var buttonExternalGivtsAdd: CustomButton!
+    @IBOutlet weak var buttonExternalGivtsSave: CustomButton!
+
+    @IBOutlet weak var viewExternalGivtsTime: CustomButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
         setupTerms()
+        setupUI()
     }
     override func viewWillAppear(_ animated: Bool) {
         if !SVProgressHUD.isVisible() {
@@ -28,18 +38,35 @@ class BudgetExternalGivtsViewController : UIViewController {
     @IBAction func backButton(_ sender: Any) {
         try? Mediater.shared.send(request: GoBackOneControllerRoute(), withContext: self)
     }
+    @IBAction func timeTapped(_ sender: Any) {
+        print("Time tapped")
+    }
+    
+    
 }
 
 extension BudgetExternalGivtsViewController {
     func setupTerms() {
         labelExternalGivtsInfo.attributedText = createInfoText()
+        labelExternalGivtsInfo.textColor = .white
+        labelExternalGivtsSubtitle.text = "BudgetExternalGiftsSubTitle".localized
+        labelExternalGivtsOrganisation.text = "BudgetExternalGiftsOrg".localized
+        labelExternalGivtsTime.text = "BudgetExternalGiftsTime".localized
+        labelExternalGivtsAmount.text = "BudgetExternalGiftsAmount".localized
+        buttonExternalGivtsAdd.setTitle("BudgetExternalGiftsAdd".localized, for: .normal)
+        buttonExternalGivtsSave.setTitle("BudgetExternalGiftsSave".localized, for: .normal)
+    }
+    func setupUI() {
+        labelExternalGivtsTimeDown.layer.addBorder(edge: .left, color: ColorHelper.UITextFieldBorderColor, thickness: 0.5)
+        viewExternalGivtsTime.borderColor = ColorHelper.UITextFieldBorderColor
+        viewExternalGivtsTime.borderWidth = 0.5
     }
 }
 
 private extension BudgetExternalGivtsViewController {
     private func createInfoText() -> NSMutableAttributedString {
         return NSMutableAttributedString()
-            .bold("BudgetExternalGiftsInfoBold".localized, font: UIFont(name: "Avenir-Black", size: 14)!)
-            .normal("BudgetExternalGiftsInfo".localized, font: UIFont(name: "Avenir-Light", size: 14)!)
+            .bold("BudgetExternalGiftsInfoBold".localized + "\n", font: UIFont(name: "Avenir-Black", size: 16)!)
+            .normal("BudgetExternalGiftsInfo".localized, font: UIFont(name: "Avenir-Light", size: 16)!)
     }
 }
