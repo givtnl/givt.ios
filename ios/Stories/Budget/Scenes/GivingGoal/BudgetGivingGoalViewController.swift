bb90186aa80890c4b03aefadd945578511011e1a
Mike Pattyn <mike@givtapp.net>
2021-05-04 14:51:18 +0200

    added picker for frequency and code for keyboard handling

--- ios/Stories/Budget/Scenes/GivingGoal/BudgetGivingGoalViewController.swift
+++ ios/Stories/Budget/Scenes/GivingGoal/BudgetGivingGoalViewController.swift
@@ -9,7 +9,10 @@
 import Foundation
 import UIKit
 import SVProgressHUD
-
+enum GivingGoalFrequency {
+    case Monthly
+    case Yearly
+}
 class BudgetGivingGoalViewController: UIViewController {
     @IBOutlet weak var navBar: UINavigationItem!
     @IBOutlet weak var infoLabel: UILabel!
@@ -18,13 +21,30 @@ class BudgetGivingGoalViewController: UIViewController {
     @IBOutlet weak var amountTitelLabel: UILabel!
     @IBOutlet weak var amountView: BudgetExternalGivtsViewWithBorder!
     @IBOutlet weak var amountViewLabelCurrency: UILabel!
+    @IBOutlet weak var amountViewTextField: TextFieldWithInset!
     
     @IBOutlet weak var periodTitelLabel: UILabel!
     @IBOutlet weak var periodView: CustomButton!
     @IBOutlet weak var periodViewLabelDown: UILabel!
+    @IBOutlet weak var periodViewTextField: TextFieldWithInset!
+    
+    @IBOutlet weak var bottomScrollViewConstraint: NSLayoutConstraint!
+    
+    var frequencyPicker: UIPickerView!
+    
+    let frequencys: Array<Array<Any>> =
+        [
+            [GivingGoalFrequency.Monthly, "BudgetExternalGiftsFrequencyMonthly".localized],
+            [GivingGoalFrequency.Yearly, "BudgetExternalGiftsFrequencyYearly".localized]
+        ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name:UIResponder.keyboardWillShowNotification, object: self.view.window)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: self.view.window)
+        
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
     }
     
     override func viewWillAppear(_ animated: Bool) {
