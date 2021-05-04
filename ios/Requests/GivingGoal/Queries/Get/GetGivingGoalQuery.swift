9b37445f7dd8352f0212c6fc71905ae9ff2bd1e4
Mike Pattyn <mike@givtapp.net>
2021-05-04 15:38:38 +0200

    added crud commands for giving goal

---
+++ ios/Requests/GivingGoal/Queries/Get/GetGivingGoalQuery.swift
@@ -0,0 +1,13 @@
+//
+//  GetGivingGoalQuery.swift
+//  ios
+//
+//  Created by Mike Pattyn on 04/05/2021.
+//  Copyright © 2021 Givt. All rights reserved.
+//
+
+import Foundation
+
+class GetGivingGoalQuery: RequestProtocol {
+    typealias TResponse = GivingGoal
+}
