b119a99c07a00d14bab2a02b99776f22f043ebdf
Mike Pattyn <mike@givtapp.net>
2021-06-04 14:22:23 +0200

    cleanup code

---
+++ ios/Stories/Budget/Scenes/YearlyOverview/BudgetYearlyOverviewViewControllerExtension.swift
@@ -0,0 +1,41 @@
+//
+//  BudgetYearlyOverviewViewControllerExtension.swift
+//  ios
+//
+//  Created by Mike Pattyn on 04/06/2021.
+//  Copyright © 2021 Givt. All rights reserved.
+//
+
+import Foundation
+
+extension BudgetYearlyOverviewViewController {
+    func setupTerms() {
+        navItem.title = year.string
+        labelGivt.text = "BudgetYearlyOverviewGivenThroughGivt".localized
+        labelNotGivt.text = "BudgetYearlyOverviewGivenThroughNotGivt".localized
+        labelTotal.text = "BudgetYearlyOverviewGivenTotal".localized
+        labelTax.text = "BudgetYearlyOverviewGivenTotalTax".localized
+    }
+    
+    func getStartDateForYear(year: Int) -> String {
+        var componentsForYearlySummaryComponents = DateComponents()
+        componentsForYearlySummaryComponents.day = 1
+        componentsForYearlySummaryComponents.month = 1
+        componentsForYearlySummaryComponents.year = year
+        let date = Calendar.current.date(from: componentsForYearlySummaryComponents)!
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        return dateFormatter.string(from: date)
+    }
+    
+    func getEndDateForYear(year: Int) -> String {
+        var componentsForYearlySummaryComponents = DateComponents()
+        componentsForYearlySummaryComponents.day = 1
+        componentsForYearlySummaryComponents.month = 1
+        componentsForYearlySummaryComponents.year = year + 1
+        let date = Calendar.current.date(from: componentsForYearlySummaryComponents)!
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        return dateFormatter.string(from: date)
+    }
+}
