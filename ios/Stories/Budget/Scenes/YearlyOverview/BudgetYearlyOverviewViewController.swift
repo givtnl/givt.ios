0c9880bdb1906371e3126565d18d5459aea57e53
Mike Pattyn <mike@givtapp.net>
2021-07-05 09:52:36 +0200

    ensure value passes is a double

--- ios/Stories/Budget/Scenes/YearlyOverview/BudgetYearlyOverviewViewController.swift
+++ ios/Stories/Budget/Scenes/YearlyOverview/BudgetYearlyOverviewViewController.swift
@@ -92,33 +92,13 @@ class BudgetYearlyOverviewViewController: BaseTrackingViewController {
             reloadData()
         }
         
-        var givtNumbers: [Int: Double] = [:]
-        var notGivtNumbers: [Int: Double] = [:]
-        
-        for int in 0...8 {
-            givtNumbers[int] = Double.random(in: 5...100)
-            notGivtNumbers[int] = Double.random(in: 5...100)
-        }
-        
-        var highestAmount = 0.0
-        for int in 0...8 {
-            let sum = givtNumbers[int]! + notGivtNumbers[int]!
-            if sum > highestAmount {
-                highestAmount = sum
-            }
-        }
-        
-        var models: [Int: MonthlyBarViewModel] = [:]
-        for int in 0...8 {
-            models[int] = MonthlyBarViewModel(givtAmount: givtNumbers[int]!, notGivtAmount: notGivtNumbers[int]!, highestAmount: highestAmount, maxBarWidth: (self.view.frame.width - 80.00), date: Date())
-            
-        }
+        let viewWidth: Double = view.frame.width - 80.00
         
         getDataForMonthBars() { bars in
             DispatchQueue.main.async {
                 bars.forEach { bar in
                     bar.highestAmount = bars.highestBarValue
-                    bar.maxBarWidth = (self.view.frame.width - 80.00)
+                    bar.maxBarWidth = viewWidth
                 }
                 self.loadMonthBars(monthBars: bars.sorted(by: { first, second in
                     first.date! < second.date!
