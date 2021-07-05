69b645ae88a5c316b0420c8b3137a8ee74cdbc42
Mike Pattyn <mike@givtapp.net>
2021-07-05 15:42:25 +0200

    load the data in the card

--- ios/Stories/Budget/Scenes/YearlyOverview/BudgetYearlyOverviewViewControllerExtension.swift
+++ ios/Stories/Budget/Scenes/YearlyOverview/BudgetYearlyOverviewViewControllerExtension.swift
@@ -41,13 +41,14 @@ extension BudgetYearlyOverviewViewController {
         
         try! Mediater.shared.sendAsync(request: GetMonthlySummaryQuery(fromDate: fromDate, tillDate: tillDate, groupType: 2, orderType: 3)) { givtModels in
             self.givtModels = givtModels
-            
             try! Mediater.shared.sendAsync(request: GetMonthlySummaryQuery(fromDate: self.getStartDateForYear(year: self.year-1), tillDate: self.getEndDateForYear(year: self.year-1), groupType: 2, orderType: 3)) { givtModelsPreviousYear in
                 self.previousYearGivtModels = givtModelsPreviousYear
             }
             
             DispatchQueue.main.async {
                 self.amountGivt.text = givtModels.map { $0.Value }.reduce(0, +).getFormattedWith(currency: UserDefaults.standard.currencySymbol, decimals: 2)
+                self.loadGivtModels(givtModels)
+
             }
             
 
@@ -62,6 +63,7 @@ extension BudgetYearlyOverviewViewController {
                     let givtAmountTax = givtModels.filter { $0.TaxDeductable != nil && $0.TaxDeductable! }.map { $0.Value }.reduce(0, +)
                     let notGivtAmountTax = notGivtModels.filter { $0.TaxDeductable != nil && $0.TaxDeductable! }.map { $0.Value }.reduce(0, +)
                     self.amountTax.text = (givtAmountTax + notGivtAmountTax).getFormattedWith(currency: UserDefaults.standard.currencySymbol, decimals: 2)
+                    self.loadNotGivtModels(notGivtModels)
                 }
                 
                 
