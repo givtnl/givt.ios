efbb68643ef9b5f1a6ba59726d83c2c507c37ab1
Mike Pattyn <mike@givtapp.net>
2021-07-06 17:33:59 +0200

    fix the menu

--- Podfile
+++ Podfile
@@ -5,14 +5,14 @@ target 'ios' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
   pod 'AppCenter'
-  pod 'SVProgressHUD', :git => 'https://github.com/givtnl/SVProgressHUD.git'
-  pod 'LGSideMenuController', '~> 2.1.1'
+  pod 'SVProgressHUD'
+  pod 'LGSideMenuController'
   pod 'PhoneNumberKit'
   pod 'SwiftClient', :git => 'https://github.com/givtnl/SwiftClient.git'
   pod 'TrustKit'
   pod 'SwipeCellKit', '~> 2.5.4'
   pod 'MaterialShowcase'
-  pod 'ReachabilitySwift', :git => 'https://github.com/givtnl/ReachabilitySwift.git'
+  pod 'ReachabilitySwift'
   pod 'AppCenter/Push'
   pod 'Mixpanel-swift'
   # Pods for ios
--- ios/Extensions/UIImageExtensions.swift
+++ ios/Extensions/UIImageExtensions.swift
@@ -22,6 +22,17 @@ extension UIImageView {
 }
 
 extension UIImage {
+    func imageWithInsets(insets: UIEdgeInsets) -> UIImage? {
+            UIGraphicsBeginImageContextWithOptions(
+                CGSize(width: self.size.width + insets.left + insets.right,
+                       height: self.size.height + insets.top + insets.bottom), false, self.scale)
+            let _ = UIGraphicsGetCurrentContext()
+            let origin = CGPoint(x: insets.left, y: insets.top)
+            self.draw(at: origin)
+            let imageWithInsets = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            return imageWithInsets
+        }
     var noir: UIImage {
         let context = CIContext(options: nil)
         let currentFilter = CIFilter(name: "CIPhotoEffectNoir")!
