ae01e95cc18846d2eeb70925d4fc2535eb50cd26
Mike Pattyn <mike@givtapp.net>
2021-07-06 13:30:49 +0200

    add empty row if no donations yet

--- ios/Stories/Budget/Scenes/YearlyOverview/BudgetYearlyOverviewViewControllerOrganisationExtension.swift
+++ ios/Stories/Budget/Scenes/YearlyOverview/BudgetYearlyOverviewViewControllerOrganisationExtension.swift
@@ -7,9 +7,22 @@
 //
 
 import Foundation
-
+extension MonthlyCardViewLine {
+    static func Empty(description: String) -> MonthlyCardViewLine {
+        let line = MonthlyCardViewLine()
+        line.collectGroupLabel.text = description
+        line.amountLabel.text = String.empty
+        return line
+    }
+}
 extension BudgetYearlyOverviewViewController {
+    
     func loadGivtModels(_ models: [MonthlySummaryDetailModel]) {
+        guard models.count > 0 else {
+            organisationGivtStackView.addArrangedSubview(MonthlyCardViewLine.Empty(description: "BudgetSummaryNoGiftsYearlyOverview".localized))
+            organisationGivtStackViewHeight.constant += 22
+            return
+        }
         models.forEach({ givtModel in
             let line = MonthlyCardViewLine()
             line.amountLabel.text = givtModel.Value.getFormattedWith(currency: UserDefaults.standard.currencySymbol, decimals: 2)
@@ -19,6 +32,11 @@ extension BudgetYearlyOverviewViewController {
         })
     }
     func loadNotGivtModels(_ models: [MonthlySummaryDetailModel]) {
+        guard models.count > 0 else {
+            organisationNotGivtStackView.addArrangedSubview(MonthlyCardViewLine.Empty(description: "BudgetSummaryNoGiftsYearlyOverview".localized))
+            organisationNotGivtStackViewHeight.constant += 22
+            return
+        }
         models.forEach({ givtModel in
             let line = MonthlyCardViewLine()
             line.amountLabel.text = givtModel.Value.getFormattedWith(currency: UserDefaults.standard.currencySymbol, decimals: 2)
