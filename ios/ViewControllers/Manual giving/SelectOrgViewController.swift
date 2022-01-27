4e817b5221aa66d09aa8fdb4f9d203a132c2dc28
Mike Pattyn <mike@givtapp.net>
2022-01-27 15:54:30 +0100

    show us organisations when hack is actibve

--- ios/ViewControllers/Manual giving/SelectOrgViewController.swift
+++ ios/ViewControllers/Manual giving/SelectOrgViewController.swift
@@ -536,8 +536,10 @@ class SelectOrgViewController: BaseScanViewController, UITableViewDataSource, UI
         }
         
         let countryFilteredList = listToLoad.filter({ (orgBeacon) -> Bool in
-            if (UserDefaults.standard.paymentType == .Undefined){
-                if (NSLocale.current.regionCode == "GB" || NSLocale.current.regionCode == "GG" || NSLocale.current.regionCode == "JE" ){
+            if UserDefaults.standard.paymentType == .Undefined{
+                if UserDefaults.standard.hackForTesting {
+                    return orgBeacon.paymentType.isCreditCard
+                }else if (NSLocale.current.regionCode == "GB" || NSLocale.current.regionCode == "GG" || NSLocale.current.regionCode == "JE" ){
                     return orgBeacon.paymentType.isBacs
                 } else if (Locale.current.regionCode == "US") {
                     return orgBeacon.paymentType.isCreditCard
