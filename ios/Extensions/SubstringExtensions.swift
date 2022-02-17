e478b474510cf19182174098c8f9fca4076ac998
Mike Pattyn <mike@givtapp.net>
2022-02-17 14:01:05 +0100

    Check QR-Code Active state and display a different message when external

---
+++ ios/Extensions/SubstringExtensions.swift
@@ -0,0 +1,15 @@
+//
+//  SubstringExtensions.swift
+//  ios
+//
+//  Created by Mike Pattyn on 17/02/2022.
+//  Copyright © 2022 Givt. All rights reserved.
+//
+
+import Foundation
+
+extension Substring {
+    func toString() -> String {
+        return String(self)
+    }
+}
---
+++ ios/ViewControllers/Give Flow/ExternalSuggestion/ExternalSuggestionViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ExternalSuggestionViewController.swift
+//  ios
+//
+//  Created by Lennie Stockman on 11/07/2018.
+//  Copyright © 2018 Givt. All rights reserved.
+//
+
+import UIKit
+import AudioToolbox
+import SafariServices
+
+class ExternalSuggestionViewController: BaseScanViewController {
+    var externalIntegrationObject: ExternalAppIntegration? = nil
+    var collectGroupDetailModel: OrgBeacon? = nil
+    var qrCode: QrCode? = nil
+    
+    var closeAction: () -> () = {}
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        externalIntegrationObject = GivtManager.shared.externalIntegration
+        
+        collectGroupDetailModel = UserDefaults.standard.orgBeaconListV2?.OrgBeacons
+            .first(where: { orgBeacon in orgBeacon.EddyNameSpace == externalIntegrationObject!.namespace })
+        
+        qrCode = collectGroupDetailModel?.QrCodes?
+            .first(where: { qrCode in qrCode.MediumId == externalIntegrationObject!.mediumId })
+               
+        setupMessageBox()
+    }
+    
+    override func safariViewController(_ controller: SFSafariViewController, initialLoadDidRedirectTo URL: URL) {
+        if let _ = URL.absoluteString.index(of: "cloud.givtapp.net") {
+            DispatchQueue.main.async {
+                self.safariViewController?.dismiss(animated: true, completion: nil)
+                self.dismiss(animated: true, completion: nil)
+                NotificationCenter.default.post(name: .GivtAmountsShouldReset, object: self)
+            }
+        }
+    }
+}
